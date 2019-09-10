08afa6c09893f2abc7ef5f4f829b893d6ced8bdd
Qi Zhang <singularitti@outlook.com>
2019-09-10 04:21:17 -0400

    Move `write_metadata` method to `SelfConsistentField`

--- src/EquationOfStateFitting.jl
+++ src/EquationOfStateFitting.jl
@@ -29,8 +29,9 @@ using SlurmWorkloadFileGenerator.Scriptify
 using SlurmWorkloadFileGenerator.Shells
 
 using Express
+using Express.SelfConsistentField: write_metadata
 
-export update_alat_press, write_input, write_metadata, prepare, finish
+export update_alat_press, write_input, prepare, finish
 
 function update_alat_press(template::PWscfInput, eos::EquationOfState, pressure::Real)
     volume = findvolume(PressureForm(), eos, pressure, (0, 1000), Order8())
@@ -54,25 +55,6 @@ function write_input(
         write(io, to_qe(object, verbose = verbose))
     end
 end # function write_input
-
-function write_metadata(output::AbstractString, object::PWscfInput, input::AbstractString)
-    metadata = Dict(
-        "outdir" => object.control.outdir,
-        "prefix" => object.control.prefix,
-        "pseudo_dir" => object.control.pseudo_dir,
-        "pseudopotentials" => [getfield(x, :pseudopotential) for x in object.atomic_species.data],
-        "input" => input
-    )
-    metadata["wfcdir"] = object.control.wf_collect ? metadata["outdir"] : object.control.wfcdir
-    metadata["wfc_namepattern"] = metadata["prefix"] * ".wfc"
-    if object.control.lkpoint_dir
-        metadata["lkpoint_dir"] = metadata["outdir"] * "/" * metadata["prefix"] * ".save"
-    end
-    lowercase(splitext(output)[2]) != ".json" && error("The file to be dumped must be a JSON file!")
-    open(output, "r+") do io
-        JSON.print(io, metadata)
-    end
-end # function write_metadata
 
 function prepare(
     step::Step{1},
--- src/SelfConsistentField.jl
+++ src/SelfConsistentField.jl
@@ -11,6 +11,25 @@ julia>
 """
 module SelfConsistentField
 
+export write_metadata
 
+function write_metadata(output::AbstractString, object::PWscfInput, input::AbstractString)
+    metadata = Dict(
+        "outdir" => object.control.outdir,
+        "prefix" => object.control.prefix,
+        "pseudo_dir" => object.control.pseudo_dir,
+        "pseudopotentials" => [getfield(x, :pseudopotential) for x in object.atomic_species.data],
+        "input" => input
+    )
+    metadata["wfcdir"] = object.control.wf_collect ? metadata["outdir"] : object.control.wfcdir
+    metadata["wfc_namepattern"] = metadata["prefix"] * ".wfc"
+    if object.control.lkpoint_dir
+        metadata["lkpoint_dir"] = metadata["outdir"] * "/" * metadata["prefix"] * ".save"
+    end
+    lowercase(splitext(output)[2]) != ".json" && error("The file to be dumped must be a JSON file!")
+    open(output, "r+") do io
+        JSON.print(io, metadata)
+    end
+end # function write_metadata
 
 end
