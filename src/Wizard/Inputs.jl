ff462f862086855c055e446a1baf356c5a27723c
Qi Zhang <singularitti@outlook.com>
2019-11-25 18:15:56 -0500

    Separate src/Wizard/Inputs.jl into modules

--- src/Wizard/Inputs.jl
+++ src/Wizard/Inputs.jl
@@ -1,35 +1,34 @@
 module Inputs
 
-using REPL
-using REPL.Terminals
-using REPL.TerminalMenus
-
-using QuantumESPRESSO: to_qe
-using QuantumESPRESSO.Namelists: Namelist
-using QuantumESPRESSO.Namelists.PHonon
-using QuantumESPRESSO.Namelists.PWscf
-using QuantumESPRESSO.Inputs.PHonon: PhInput, Q2rInput, MatdynInput, DynmatInput
-using QuantumESPRESSO.Inputs.PWscf: PWInput
-using Rematch: @match
-using Setfield: PropertyLens, set
-
-using ..Namelists
-
 export input_helper
 
-function input_helper(terminal::TTYTerminal, ::Type{T}) where {T<:PWInput}
-    control = namelist_helper(terminal, PWscf.ControlNamelist)
-    system = namelist_helper(terminal, PWscf.SystemNamelist)
-    electrons = namelist_helper(terminal, PWscf.ElectronsNamelist)
+function input_helper end
+
+module PWscf
+
+using REPL.Terminals: TTYTerminal
+using REPL.TerminalMenus: RadioMenu, request
+
+using QuantumESPRESSO.Namelists.PWscf:
+    ControlNamelist, SystemNamelist, ElectronsNamelist, IonsNamelist, CellNamelist
+using QuantumESPRESSO.Inputs.PWscf: PWInput
+
+using ...Namelists: namelist_helper
+using ..Inputs
+
+function Inputs.input_helper(terminal::TTYTerminal, ::Type{T}) where {T<:PWInput}
+    control = namelist_helper(terminal, ControlNamelist)
+    system = namelist_helper(terminal, SystemNamelist)
+    electrons = namelist_helper(terminal, ElectronsNamelist)
     ions = if control.calculation ∈ ("relax", "md", "vc-relax", "vc-md")
-        namelist_helper(terminal, PWscf.IonsNamelist)
+        namelist_helper(terminal, IonsNamelist)
     else
-        PWscf.IonsNamelist()
+        IonsNamelist()
     end
     cell = if control.calculation ∈ ("vc-relax", "vc-md")
-        namelist_helper(terminal, PWscf.CellNamelist)
+        namelist_helper(terminal, CellNamelist)
     else
-        PWscf.CellNamelist()
+        CellNamelist()
     end
     return T(
         control,
@@ -39,16 +38,34 @@ function input_helper(terminal::TTYTerminal, ::Type{T}) where {T<:PWInput}
         cell = cell,
     )
 end # function input_helper
-function input_helper(terminal::TTYTerminal, ::Type{T}) where {T<:PhInput}
-    ph = namelist_helper(terminal, PHonon.PhNamelist)
+
+end # module PWscf
+
+module PHonon
+
+using REPL.Terminals: TTYTerminal
+using REPL.TerminalMenus: RadioMenu, request
+
+using QuantumESPRESSO.Namelists.PHonon:
+    PhNamelist, Q2rNamelist, MatdynNamelist, DynmatNamelist
+using QuantumESPRESSO.Inputs.PHonon: PhInput, Q2rInput, MatdynInput, DynmatInput
+
+using ...Namelists: namelist_helper
+using ..Inputs
+
+function Inputs.input_helper(terminal::TTYTerminal, ::Type{T}) where {T<:PhInput}
+    ph = namelist_helper(terminal, PhNamelist)
 end
-function input_helper(terminal::TTYTerminal, ::Type{T}) where {T<:Q2rInput}
-    ph = namelist_helper(terminal, PHonon.Q2rNamelist)
+function Inputs.input_helper(terminal::TTYTerminal, ::Type{T}) where {T<:Q2rInput}
+    ph = namelist_helper(terminal, Q2rNamelist)
 end
-function input_helper(terminal::TTYTerminal, ::Type{T}) where {T<:MatdynInput}
-    ph = namelist_helper(terminal, PHonon.MatdynNamelist)
+function Inputs.input_helper(terminal::TTYTerminal, ::Type{T}) where {T<:MatdynInput}
+    ph = namelist_helper(terminal, MatdynNamelist)
 end
-function input_helper(terminal::TTYTerminal, ::Type{T}) where {T<:DynmatInput}
-    ph = namelist_helper(terminal, PHonon.DynmatNamelist)
+function Inputs.input_helper(terminal::TTYTerminal, ::Type{T}) where {T<:DynmatInput}
+    ph = namelist_helper(terminal, DynmatNamelist)
 end
+
+end # module PHonon
+
 end
