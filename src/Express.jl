608e781e29bedbd39812d527bbf814a6ad9589b4
Qi Zhang <singularitti@outlook.com>
2019-11-29 16:51:36 -0500

    Add new types

--- src/Express.jl
+++ src/Express.jl
@@ -1,9 +1,14 @@
 module Express
 
-export Step
+export Step, PWscfCalculation, PHononCalculation, CPCalculation
 
 struct Step{N} end
 Step(n) = Step{n}()
+
+abstract type QuantumESPRESSOCalculation end
+struct PWscfCalculation{T} <: QuantumESPRESSOCalculation end
+struct PHononCalculation{T} <: QuantumESPRESSOCalculation end
+struct CPCalculation{T} <: QuantumESPRESSOCalculation end
 
 include("Jobs.jl")
 include("SelfConsistentField.jl")
