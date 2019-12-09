a1bf1031ce03ba5c513175984d77929f13b030fe
Qi Zhang <singularitti@outlook.com>
2019-12-08 20:00:43 -0800

    Deprecate `SubJob`, use `Future` directly
    `Future`s contain the id of each process

--- src/Jobs.jl
+++ src/Jobs.jl
@@ -11,7 +11,7 @@ using QuantumESPRESSOBase.CLI
 
 using Express
 
-export MpiCmd, Job, SubJob
+export MpiCmd, Job
 export nprocs_per_subjob, distribute_process, isjobdone, fetch_results
 
 @with_kw struct MpiCmd
@@ -29,11 +29,6 @@ end
     directives::Dict = Dict{String,Any}()
 end
 
-struct SubJob
-    worker_id::Int
-    ref::Future
-end
-
 function nprocs_per_subjob(total_num::Int, nsubjob::Int)
     quotient, remainder = divrem(total_num, nsubjob)
     if remainder != 0
@@ -45,21 +40,21 @@ end # function nprocs_per_subjob
 function distribute_process(cmd::MpiCmd, worker_ids = workers())
     # mpirun -np $n pw.x -in $in -out $out
     # Similar to `invoke_on_workers` in https://cosx.org/2017/08/distributed-learning-in-julia
-    subjobs = Vector{SubJob}(undef, nworkers())
+    refs = Vector{Future}(undef, nworkers())
     @set! cmd.np = nprocs_per_subjob(cmd.np, length(worker_ids))
     for (i, id) in enumerate(worker_ids)
-        subjobs[i] = SubJob(id, @spawnat id run(commandify(cmd)))
+        refs[i] = @spawnat id run(commandify(cmd))
     end
-    return subjobs
+    return refs
 end # function distribute_process
 
-function isjobdone(subjobs::AbstractArray{SubJob})
-    return all(map(x -> isready(x.ref), subjobs))
+function isjobdone(refs::AbstractArray{Future})
+    return all(map(x -> isready(x), refs))
 end # function isjobdone
 
-function fetch_results(subjobs::AbstractArray{SubJob})
-    return map(subjobs) do x
-        isready(x.ref) ? fetch(x.ref) : nothing
+function fetch_results(refs::AbstractArray{Future})
+    return map(refs) do x
+        isready(x) ? fetch(x) : nothing
     end
 end # function fetch_results
 
