5eefedffd147728c91629b961727224fe69b530a
singularitti <singularitti@outlook.com>
2020-05-24 02:09:05 -0400

    Rename variables in examples/GaN.jl

--- examples/GaN.jl
+++ examples/GaN.jl
@@ -1,13 +1,16 @@
 using Distributed
 addprocs(8)
-using Express, Express.EosFitting, Express.Jobs, Express.CLI
-using EquationsOfState.NonlinearFitting, EquationsOfState.Collections, EquationsOfState.Find
-using QuantumESPRESSO.Inputs.PWscf, QuantumESPRESSO.CLI
-using QuantumESPRESSOParsers
-using Unitful, UnitfulAtomic
-using DockerPy.Client, DockerPy.Images, DockerPy.Containers
+@everywhere using Pkg
+@everywhere Pkg.activate(".")
+@everywhere using Express, Express.EosFitting, Express.Jobs, Express.CLI
+@everywhere using EquationsOfState.NonlinearFitting,
+    EquationsOfState.Collections, EquationsOfState.Find
+@everywhere using QuantumESPRESSO.Inputs.PWscf, QuantumESPRESSOBase.CLI
+@everywhere using QuantumESPRESSOParsers
+@everywhere using Unitful, UnitfulAtomic
+@everywhere using DockerPy.Client, DockerPy.Images, DockerPy.Containers
 
-scf = raw"""
+@everywhere str = raw"""
 &control
 calculation='scf'
 verbosity = 'high'
@@ -42,59 +45,79 @@ K_POINTS automatic
 6 6 6 0 0 0
 """
 
+@everywhere docker = DockerClient()
+@everywhere image = pull(docker, "rinnocente/qe-full-6.2.1")[1]
+# @everywhere begin
+#     container = Container(
+#         docker,
+#         image,
+#         command = "sh",
+#         name = "qe",
+#         tty = true,
+#         stdin_open = true,
+#         volumes = Dict(
+#             joinpath(pwd(), "examples") =>
+#                 Dict("bind" => "/home/qe/test", "mode" => "rw"),
+#         ),
+#     )
+# end
+@everywhere container = containers(docker)[1]
+start(container)
+exec_run(container, "mkdir -p /home/qe/pseudo/")
+exec_run(
+    container,
+    "wget -O /home/qe/pseudo/Ga.pbe-dn-kjpaw_psl.1.0.0.UPF https://www.quantum-espresso.org/upf_files/Ga.pbe-dn-kjpaw_psl.1.0.0.UPF",
+)
+exec_run(
+    container,
+    "wget -O /home/qe/pseudo/N.pbe-n-kjpaw_psl.1.0.0.UPF https://www.quantum-espresso.org/upf_files/N.pbe-n-kjpaw_psl.1.0.0.UPF",
+)
 # ================================================================= Step 1 =============================
 # pressures = [-100, -50, 0, 50, 100, 150, 200, 250, 300, 350, 400, 500] * u"kbar"
-pressures = [-50, 0, 50, 100, 150, 200, 250, 300] * u"kbar"
-scfdirs = map(x -> mkpath("examples/scf$(ustrip(x))"), pressures)
-inputs = map(x -> x * "/scf.in", scfdirs)
-crude_eos = BirchMurnaghan3rd(317 * u"bohr^3", 210 * u"GPa", 4, -612.43 * u"Ry")
-template = parse(PWInput, scf)
-Step(1)(inputs, template, crude_eos, pressures)
+@everywhere pressures = [-50, 0, 50, 100, 150, 200, 250, 300] * u"kbar"
+@everywhere scfdirs_local = map(x -> mkpath("examples/scf$(ustrip(x))"), pressures)
+@everywhere scfinputs_local = map(x -> x * "/scf.in", scfdirs_local)
+@everywhere crude_eos = BirchMurnaghan3rd(317 * u"bohr^3", 210 * u"GPa", 4, -612.43 * u"Ry")
+@everywhere template = parse(PWInput, str)
+Step(1)(scfinputs_local, template, crude_eos, pressures)
+@everywhere scfinputs_docker = map(x -> "/home/qe/test/scf$(ustrip(x))/scf.in", pressures)
+@everywhere scfoutputs_docker = map(x -> replace(x, ".in" => ".out"), scfinputs_docker)
 # ================================================================= Step 2 =============================
-docker = DockerClient()
-image = pull(docker.images, "rinnocente/qe-full-6.2.1")[1]
-container = Container(docker, image, command = "sh", name = "qe", tty = true, stdin_open = true, volumes = Dict(joinpath(pwd(), "examples") => Dict("bind" => "/home/qe/test", "mode" => "rw")))
-start(container)
-inputs_ondocker = map(x -> "/home/qe/test/scf$(ustrip(x))/scf.in", pressures)
-scf_outputs = map(x -> replace(x, ".in" => ".out"), inputs_ondocker)
-exec_run(container, "mkdir -p /home/qe/pseudo/")
-exec_run(container, "wget -O /home/qe/pseudo/Ga.pbe-dn-kjpaw_psl.1.0.0.UPF https://www.quantum-espresso.org/upf_files/Ga.pbe-dn-kjpaw_psl.1.0.0.UPF")
-exec_run(container, "wget -O /home/qe/pseudo/N.pbe-n-kjpaw_psl.1.0.0.UPF https://www.quantum-espresso.org/upf_files/N.pbe-n-kjpaw_psl.1.0.0.UPF")
-bag = Step(2)(
-    inputs_ondocker,
-    scf_outputs,
+@everywhere bag = Step(2)(
+    scfinputs_docker,
+    scfoutputs_docker,
     16,
-    PWExec(which = "/home/qe/qe-6.2.1/bin/pw.x"),
+    pwcmd(bin = "/home/qe/qe-6.2.1/bin/pw.x"),
     workers(),
     isdocker = true,
     container = container,
 )
-map(bag, map(x -> replace(x, ".in" => ".out"), inputs)) do x, outfile
+map(bag, map(x -> replace(x, ".in" => ".out"), scfinputs_local)) do x, outfile
     touch(outfile)
     open(outfile, "r+") do io
         write(io, x.output[1])
     end
 end
 # ================================================================= Step 3: read scf.out and curve-fitting =============================
-new_eos = Step(3)(map(x -> replace(x, ".in" => ".out"), inputs), crude_eos)
+new_eos = Step(3)(map(x -> replace(x, ".in" => ".out"), scfinputs_local), crude_eos)
 # new eos:  317.75905077576425 a₀^3, 172.89506496025282 GPa, 4.357510886414555, -612.4315102681139 Ry
 # ================================================================= Step 4 =============================
-vcdirs = map(x -> mkpath("examples/vc$(ustrip(x))"), pressures)
-vcinputs = map(x -> x * "/vc.in", vcdirs)
-Step(4)(vcinputs, template, new_eos, pressures)
+vcdirs_local = map(x -> mkpath("examples/vc$(ustrip(x))"), pressures)
+vcinputs_local = map(x -> x * "/vc.in", vcdirs_local)
+Step(4)(vcinputs_local, template, new_eos, pressures)
 # ================================================================= Step 5 =============================
-vcinputs_ondocker = map(x -> replace(x, "scf" => "vc"), inputs_ondocker)
-output_vc = map(x -> replace(x, ".in" => ".out"), vcinputs)
+vcinputs_docker = map(x -> replace(x, "scf" => "vc"), scfinputs_docker)
+vcoutput_docker = map(x -> replace(x, ".in" => ".out"), vcinputs_local)
 bag2 = Step(5)(
-    vcinputs_ondocker,
-    output_vc,
+    vcinputs_docker,
+    vcoutput_docker,
     16,
-    PWExec(which = "/home/qe/qe-6.2.1/bin/pw.x"),
+    pwcmd(bin = "/home/qe/qe-6.2.1/bin/pw.x"),
     workers(),
     isdocker = true,
     container = container,
 )
-map(bag2, map(x -> replace(x, ".in" => ".out"), vcinputs)) do x, outfile
+map(bag2, map(x -> replace(x, ".in" => ".out"), vcinputs_local)) do x, outfile
     touch(outfile)
     open(outfile, "r+") do io
         write(io, x.output[1])
@@ -102,6 +125,6 @@ map(bag2, map(x -> replace(x, ".in" => ".out"), vcinputs)) do x, outfile
 end
 stop(container)
 # ================================================================= Step 6: read vcrelax.out file and curve-fitting =============================
-result = Step(6)(output_vc, new_eos)
-println(result)
 # final eos: 317.7711705399742 a₀^3, 172.8125730578396 GPa, 4.3535165337769195, -612.4315134858152 Ry
+result = Step(6)(vcoutput_docker, new_eos)
+println(result)
