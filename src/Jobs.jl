3441e6c6eb66b25d8ca662d2c167f5e1c3424317
singularitti <singularitti@outlook.com>
2020-06-07 04:05:00 -0400

    Redefine `distributejobs`

--- src/Jobs.jl
+++ src/Jobs.jl
@@ -7,11 +7,7 @@ using DockerPy.Containers
 using ..Environments: DockerEnvironment, LocalEnvironment
 
 export JobStatus
-export nprocs_task,
-    distributejobs,
-    isjobdone,
-    jobresult,
-    jobstatus
+export nprocs_task, distributejobs, isjobdone, jobresult, jobstatus
 
 @enum JobStatus begin
     RUNNING
@@ -28,17 +24,13 @@ function nprocs_task(total_num, nsubjob)
 end # function nprocs_task
 
 function distributejobs(cmds, ::LocalEnvironment)
-    # Similar to `invoke_on_workers` in https://cosx.org/2017/08/distributed-learning-in-julia
-    ids = addprocs(length(cmds))
-    return map(cmds, ids) do cmd, id  # promises
-        @spawnat id run(cmd; wait = true)  # Must wait, or else lose I/O streams
+    return map(cmds) do cmd
+        @async run(cmd; wait = true)  # Must wait, or else lose I/O streams
     end
 end # function distributejobs
 function distributejobs(cmds, environment::DockerEnvironment)
-    # Similar to `invoke_on_workers` in https://cosx.org/2017/08/distributed-learning-in-julia
-    ids = addprocs(length(cmds))
-    return map(cmds, ids) do cmd, id  # promises
-        exec_run(environment.container, cmd; demux = true)
+    return map(cmds) do cmd
+        @async exec_run(environment.container, cmd; demux = true)
     end
 end # function distributejobs
 
