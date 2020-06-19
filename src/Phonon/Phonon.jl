2f4213e542d1f2366b6faa48b74049a80af02fde
singularitti <singularitti@outlook.com>
2020-06-19 08:20:41 -0400

    Move functions to src/Phonon/QuantumESPRESSO.jl

--- src/Phonon/Phonon.jl
+++ src/Phonon/Phonon.jl
@@ -11,17 +11,8 @@ julia>
 """
 module Phonon
 
-using ConstructionBase: setproperties
-using QuantumESPRESSO.Inputs: InputFile, qestring
-using QuantumESPRESSO.Inputs.PWscf:
-    AtomicPositionsCard, CellParametersCard, PWInput, optconvert
-using QuantumESPRESSO.Inputs.PHonon: PhInput, Q2rInput, MatdynInput, DynmatInput
-using QuantumESPRESSO.Outputs: OutputFile
-using QuantumESPRESSO.Outputs.PWscf: parsefinal
-using QuantumESPRESSO.Setters: CellParametersSetter, VerbositySetter
-using Setfield: @set!
-
 using ..Express:
+    Step,
     Calculation,
     SelfConsistentField,
     DfptMethod,
@@ -30,89 +21,13 @@ using ..Express:
     LAUNCH_JOB,
     ANALYSE_OUTPUT
 
-export set_structure, relay, preprocess
+export set_structure, relay, Step
 
-"""
-    set_structure(output::AbstractString, template::PWInput)
+function set_structure end
 
-Read structure information from `output`, and update related fields of `template`.
-"""
-function set_structure(output::AbstractString, template::PWInput)
-    str = read(OutputFile(output))
-    cell = parsefinal(CellParametersCard{Float64}, str)
-    return setproperties(
-        template,
-        atomic_positions = parsefinal(AtomicPositionsCard, str),
-        cell_parameters = CellParametersCard(cell.data / template.system.celldm[1], "alat"), # The result of `parsefinal` must be a `CellParametersCard` with `"bohr"` or `"angstrom"` option, convert it to "bohr" by default
-    )
-end # function set_structure
+function preset end
 
-# This is a helper function and should not be exported.
-function preset(template::PWInput)
-    @set! template.control.verbosity = "high"
-    @set! template.control.wf_collect = true
-    @set! template.control.tstress = true
-    @set! template.control.tprnfor = true
-    @set! template.control.disk_io = "high"
-    @set! template.control.calculation = "scf"
-    return template
-end # function preset
-function preset(template::PhInput)
-    @set! template.inputph.verbosity = "high"
-    return template
-end # function preset
-
-"""
-    relay(from::PWInput, to::PhInput)
-
-Relay shared information from a `PWInput` to a `PhInput`.
-
-A `PWInput` before a `PhInput` has the information of `outdir` and `prefix`. They must keep the same in a
-phonon calculation.
-"""
-function relay(pw::PWInput, ph::PhInput)
-    @set! ph.inputph.outdir = pw.control.outdir
-    @set! ph.inputph.prefix = pw.control.prefix
-    return ph
-end # function relay
-"""
-    relay(from::PhInput, to::Q2rInput)
-
-Relay shared information from a `PhInput` to a `Q2rInput`.
-
-A `PhInput` before a `Q2rInput` has the information of `fildyn`. It must keep the same in a q2r calculation.
-"""
-function relay(ph::PhInput, q2r::Q2rInput)
-    @set! q2r.input.fildyn = ph.inputph.fildyn
-    return q2r
-end # function relay
-"""
-    relay(from::Q2rInput, to::MatdynInput)
-
-Relay shared information from a `Q2rInput` to a `MatdynInput`.
-
-A `Q2rInput` before a `MatdynInput` has the information of `fildyn`, `flfrc` and `loto_2d`. They must keep the same
-in a matdyn calculation.
-"""
-function relay(q2r::Q2rInput, matdyn::MatdynInput)
-    @set! matdyn.input.flfrc = q2r.input.flfrc
-    @set! matdyn.input.loto_2d = q2r.input.loto_2d
-    return matdyn
-end # function relay
-"""
-    relay(from::PhInput, to::DynmatInput)
-
-Relay shared information from a `PhInput` to a `DynmatInput`.
-
-A `PhInput` before a `DynmatInput` has the information of `asr`, `fildyn` and `amass`. They must keep the same
-in a dynmat calculation.
-"""
-function relay(ph::PhInput, dynmat::DynmatInput)
-    # @set! dynmat.input.asr = ph.inputph.asr  # TODO
-    @set! dynmat.input.fildyn = ph.inputph.fildyn
-    @set! dynmat.input.amass = ph.inputph.amass
-    return dynmat
-end # function relay
+function relay end
 
 function (::Step{SelfConsistentField,PREPARE_INPUT})(
     scf_inputs,
@@ -184,7 +99,5 @@ function preprocess(
     end
     return
 end
-
-#???
 
 end
---
+++ src/Phonon/QuantumESPRESSO.jl
@@ -0,0 +1,91 @@
+module QuantumESPRESSO
+
+using QuantumESPRESSO.Inputs: InputFile, inputstring
+using QuantumESPRESSO.Inputs.PWscf:
+    AtomicPositionsCard, CellParametersCard, PWInput, optconvert
+using QuantumESPRESSO.Inputs.PHonon: PhInput, Q2rInput, MatdynInput, DynmatInput
+using QuantumESPRESSO.Outputs.PWscf: parsefinal
+using Setfield: @set!
+
+import Express.Phonon: set_structure, preset
+
+function set_structure(output::AbstractString, template::PWInput)
+    str = open(output, "r") do io
+        read(output, String)
+    end
+    cell = parsefinal(CellParametersCard{Float64}, str)
+    return setproperties(
+        template,
+        atomic_positions = parsefinal(AtomicPositionsCard, str),
+        cell_parameters = CellParametersCard(cell.data / template.system.celldm[1], "alat"), # The result of `parsefinal` must be a `CellParametersCard` with `"bohr"` or `"angstrom"` option, convert it to "bohr" by default
+    )
+end # function set_structure
+
+# This is a helper function and should not be exported.
+function preset(template::PWInput)
+    @set! template.control.verbosity = "high"
+    @set! template.control.wf_collect = true
+    @set! template.control.tstress = true
+    @set! template.control.tprnfor = true
+    @set! template.control.disk_io = "high"
+    @set! template.control.calculation = "scf"
+    return template
+end # function preset
+function preset(template::PhInput)
+    @set! template.inputph.verbosity = "high"
+    return template
+end # function preset
+
+"""
+    relay(from::PWInput, to::PhInput)
+
+Relay shared information from a `PWInput` to a `PhInput`.
+
+A `PWInput` before a `PhInput` has the information of `outdir` and `prefix`. They must keep the same in a
+phonon calculation.
+"""
+function relay(pw::PWInput, ph::PhInput)
+    @set! ph.inputph.outdir = pw.control.outdir
+    @set! ph.inputph.prefix = pw.control.prefix
+    return ph
+end # function relay
+"""
+    relay(from::PhInput, to::Q2rInput)
+
+Relay shared information from a `PhInput` to a `Q2rInput`.
+
+A `PhInput` before a `Q2rInput` has the information of `fildyn`. It must keep the same in a q2r calculation.
+"""
+function relay(ph::PhInput, q2r::Q2rInput)
+    @set! q2r.input.fildyn = ph.inputph.fildyn
+    return q2r
+end # function relay
+"""
+    relay(from::Q2rInput, to::MatdynInput)
+
+Relay shared information from a `Q2rInput` to a `MatdynInput`.
+
+A `Q2rInput` before a `MatdynInput` has the information of `fildyn`, `flfrc` and `loto_2d`. They must keep the same
+in a matdyn calculation.
+"""
+function relay(q2r::Q2rInput, matdyn::MatdynInput)
+    @set! matdyn.input.flfrc = q2r.input.flfrc
+    @set! matdyn.input.loto_2d = q2r.input.loto_2d
+    return matdyn
+end # function relay
+"""
+    relay(from::PhInput, to::DynmatInput)
+
+Relay shared information from a `PhInput` to a `DynmatInput`.
+
+A `PhInput` before a `DynmatInput` has the information of `asr`, `fildyn` and `amass`. They must keep the same
+in a dynmat calculation.
+"""
+function relay(ph::PhInput, dynmat::DynmatInput)
+    # @set! dynmat.input.asr = ph.inputph.asr  # TODO
+    @set! dynmat.input.fildyn = ph.inputph.fildyn
+    @set! dynmat.input.amass = ph.inputph.amass
+    return dynmat
+end # function relay
+
+end
