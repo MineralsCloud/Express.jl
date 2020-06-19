b0681fed80dcb6368736391bba8fb9a739486edc
singularitti <singularitti@outlook.com>
2020-06-19 04:32:28 -0400

    Rewrite `preset` & `relay` use only `@set!`
    Remove other `Setfield` & `Kaleido`

--- src/Phonon.jl
+++ src/Phonon.jl
@@ -12,7 +12,6 @@ julia>
 module Phonon
 
 using ConstructionBase: setproperties
-using Kaleido: @batchlens
 using QuantumESPRESSO.Inputs: InputFile, qestring
 using QuantumESPRESSO.Inputs.PWscf:
     AtomicPositionsCard, CellParametersCard, PWInput, optconvert
@@ -20,16 +19,11 @@ using QuantumESPRESSO.Inputs.PHonon: PhInput, Q2rInput, MatdynInput, DynmatInput
 using QuantumESPRESSO.Outputs: OutputFile
 using QuantumESPRESSO.Outputs.PWscf: parsefinal
 using QuantumESPRESSO.Setters: CellParametersSetter, VerbositySetter
-using Setfield: get, set, @lens, @set!
+using Setfield: @set!
 
 using ..Express: Calculation, SelfConsistentField, PREPARE_INPUT, LAUNCH_JOB, ANALYSE_OUTPUT
 
 export set_structure, relay, preprocess
-
-struct DensityFunctionalPertubation{T} <: Calculation{T} end
-struct ReciprocalToReal{T} <: Calculation{T} end
-struct PhononDensityOfStates{T} <: Calculation{T} end
-struct PhononDispersion{T} <: Calculation{T} end
 
 """
     set_structure(output::AbstractString, template::PWInput)
@@ -48,18 +42,16 @@ end # function set_structure
 
 # This is a helper function and should not be exported.
 function preset(template::PWInput)
-    @set! template.control = set(template.control, VerbositySetter(:high))
+    @set! template.control.verbosity = "high"
+    @set! template.control.wf_collect = true
+    @set! template.control.tstress = true
+    @set! template.control.tprnfor = true
+    @set! template.control.disk_io = "high"
     @set! template.control.calculation = "scf"
-    template = set(template, CellParametersSetter())
-    @set! template.system.ibrav = 0
     return template
 end # function preset
 function preset(template::PhInput)
-    lenses = @batchlens(begin
-        _.inputph.verbosity  # Get the `template`'s `phonon.calculation` value
-    end)
-    # Set the `template`'s values with...
-    template = set(template, lenses, ("high",))
+    @set! template.inputph.verbosity = "high"
 end # function preset
 
 """
@@ -70,12 +62,10 @@ Relay shared information from a `PWInput` to a `PhInput`.
 A `PWInput` before a `PhInput` has the information of `outdir` and `prefix`. They must keep the same in a
 phonon calculation.
 """
-function relay(from::PWInput, to::PhInput)
-    lenses = @batchlens(begin
-        _.outdir
-        _.prefix
-    end)
-    return set(to, (@lens _.inputph) ∘ lenses, get(from, (@lens _.control) ∘ lenses))
+function relay(pw::PWInput, ph::PhInput)
+    @set! ph.inputph.outdir = pw.control.outdir
+    @set! ph.inputph.prefix = pw.control.prefix
+    return ph
 end # function relay
 """
     relay(from::PhInput, to::Q2rInput)
@@ -84,9 +74,9 @@ Relay shared information from a `PhInput` to a `Q2rInput`.
 
 A `PhInput` before a `Q2rInput` has the information of `fildyn`. It must keep the same in a q2r calculation.
 """
-function relay(from::PhInput, to::Q2rInput)
-    fildyn = @lens _.fildyn
-    return set(to, (@lens _.input) ∘ fildyn, get(from, (@lens _.inputph) ∘ fildyn))
+function relay(ph::PhInput, q2r::Q2rInput)
+    @set! q2r.input.fildyn = ph.inputph.fildyn
+    return q2r
 end # function relay
 """
     relay(from::Q2rInput, to::MatdynInput)
@@ -96,12 +86,9 @@ Relay shared information from a `Q2rInput` to a `MatdynInput`.
 A `Q2rInput` before a `MatdynInput` has the information of `fildyn`, `flfrc` and `loto_2d`. They must keep the same
 in a matdyn calculation.
 """
-function relay(from::Q2rInput, to::MatdynInput)
-    lenses = @batchlens(begin
-        _.input.flfrc
-        _.input.loto_2d
-    end)
-    return set(to, lenses, get(from, lenses))
+function relay(q2r::Q2rInput, matdyn::MatdynInput)
+    @set! matdyn.input.flfrc = q2r.input.flfrc
+    @set! matdyn.input.loto_2d = q2r.input.loto_2d
 end # function relay
 """
     relay(from::PhInput, to::DynmatInput)
@@ -111,13 +98,11 @@ Relay shared information from a `PhInput` to a `DynmatInput`.
 A `PhInput` before a `DynmatInput` has the information of `asr`, `fildyn` and `amass`. They must keep the same
 in a dynmat calculation.
 """
-function relay(from::PhInput, to::DynmatInput)
-    lenses = @batchlens(begin
-        #_.asr   #TODO
-        _.fildyn
-        _.amass
-    end)
-    return set(to, (@lens _.input) ∘ lenses, get(from, (@lens _.inputph) ∘ lenses))
+function relay(ph::PhInput, dynmat::DynmatInput)
+    # @set! dynmat.input.asr = ph.inputph.asr  # TODO
+    @set! dynmat.input.fildyn = ph.inputph.fildyn
+    @set! dynmat.input.amass = ph.inputph.amass
+    return dynmat
 end # function relay
 
 """
