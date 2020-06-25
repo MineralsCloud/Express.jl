960cb1ab7bc566dcc733759c69d377fc81026369
singularitti <singularitti@outlook.com>
2020-06-25 06:33:37 -0400

    Move `MpiCmd` to AbInitioSoftwareBase.jl

--- src/CLI.jl
+++ src/CLI.jl
@@ -1,29 +1,5 @@
 module CLI
 
-export MpiCmd
 
-struct MpiCmd
-    n::UInt
-    bin
-    host
-    arch
-    wdir
-    path
-    file
-    configfile
-    env
-end
-MpiCmd(
-    n;
-    bin = "mpiexec",
-    host = "",
-    arch = "",
-    wdir = "",
-    path = String[],
-    file = "",
-    configfile = "",
-    env = ENV,
-) = MpiCmd(n, bin, host, arch, wdir, path, file, configfile, env)
-# The docs are from https://www.mpich.org/static/docs/v3.3/www1/mpiexec.html.
 
 end # module CLI
