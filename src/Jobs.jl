b36239b315bf9eb9334453ce0405a04bca88f579
singularitti <singularitti@outlook.com>
2020-07-05 17:24:47 -0400

    Rewrite `JobTracker` & `launchjob`, etc.

--- src/Jobs.jl
+++ src/Jobs.jl
@@ -4,50 +4,82 @@ using Dates: DateTime, CompoundPeriod, now, canonicalize
 using Distributed
 using DockerPy.Containers
 
-export div_nprocs, launchjob, running, succeeded, failed
+export div_nprocs, launchjob, running, succeeded, failed, starttime, endtime, usetime
 
 struct JobTracker
-    subjobs
-    starttime::DateTime
-    JobTracker(subjobs) = new(subjobs, now())
+    subjobs::Vector{Future}
+    starttime::Vector{DateTime}
+    endtime::Vector{DateTime}
 end
 
-function div_nprocs(total_num, nsubjob)
-    quotient, remainder = divrem(total_num, nsubjob)
-    if !iszero(remainder)
-        @warn "The processes are not fully balanced! Consider the number of subjobs!"
-    end
-    return quotient
-end # function div_nprocs
-
 function launchjob(cmds, interval = 3)
-    return JobTracker(map(cmds) do cmd
+    tracker =
+        JobTracker(similar(cmds, Future), similar(cmds, DateTime), similar(cmds, DateTime))
+    for (i, cmd) in enumerate(cmds)
         sleep(interval)
-        @spawn run(cmd; wait = true)  # Must wait, or else lose I/O streams
-    end)
+        tracker.subjobs[i] = @spawn begin
+            tracker.starttime[i] = now()
+            x = run(cmd; wait = true)
+            tracker.endtime[i] = now()
+            x
+        end
+    end
+    return tracker
 end # function launchjob
 
 running(x::JobTracker) = filter(!isready, x.subjobs)
+
 succeeded(x::JobTracker) = filter(success, map(fetch, filter(isready, x.subjobs)))
+
 failed(x::JobTracker) = filter(!success, map(fetch, filter(isready, x.subjobs)))
+
+starttime(x::JobTracker) = x.starttime
+
+function endtime(x::JobTracker)
+    return map(x.subjobs, x.endtime) do subjob, endtime
+        if isready(subjob)
+            endtime
+        else
+            nothing
+        end
+    end
+end # function endtime
+
+function usetime(x::JobTracker)
+    return map(x.subjobs, x.endtime, x.starttime) do subjob, endtime, starttime
+        if isready(subjob)
+            endtime - starttime
+        else
+            nothing
+        end
+    end
+end # function usetime
 
 function Base.show(io::IO, x::JobTracker)
     print(
         io,
         join(
             (
-                "\033[34mtime elapsed:\033[0m ",  # Green text
-                canonicalize(CompoundPeriod(now() - x.starttime)),
-                "\033[34mrunning:\033[0m ",
+                "\033[34mrunning:\033[0m ",  # Blue text
                 running(x),
-                "\033[32msucceeded:\033[0m ",
+                "\033[32msucceeded:\033[0m ",  # Green text
                 succeeded(x),
-                "\033[31mfailed:\033[0m ",
+                "\033[31mfailed:\033[0m ",  # Red text
                 failed(x),
+                "\033[34mtime used:\033[0m ",  # Green text
+                canonicalize.(CompoundPeriod.(usetime(x))),
             ),
             '\n',
         ),
     )
 end # function Base.show
+
+function div_nprocs(np, nj)
+    quotient, remainder = divrem(np, nj)
+    if !iszero(remainder)
+        @warn "The processes are not fully balanced! Consider the number of subjobs!"
+    end
+    return quotient
+end # function div_nprocs
 
 end
