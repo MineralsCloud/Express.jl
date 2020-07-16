bbdbdf34fcf9eb130d7288875efb66f7498d5f58
singularitti <singularitti@outlook.com>
2020-07-16 12:45:26 -0400

    Add back `Action` & `Step` types in Express.jl

--- src/Express.jl
+++ src/Express.jl
@@ -24,6 +24,18 @@ struct ForceConstant <: VibrationalProperty end
 struct PhononDispersion <: VibrationalProperty end
 struct PhononDensityOfStates <: VibrationalProperty end
 
+struct Action{T} end
+
+struct Step{S<:Calculation,T<:Action} end
+Step(c::Calculation, a::Action) = Step{typeof(c),typeof(a)}()
+(c::Calculation)(a::Action) = Step(c, a)
+
+calctype(::Type{Step{S,T}}) where {S,T} = S
+calctype(s::Step) = calctype(typeof(s))  # No instance, `S` could be abstract
+
+actiontype(::Type{Step{S,T}}) where {S,T} = T
+actiontype(s::Step) = actiontype(typeof(s))
+
 _uparse(str::AbstractString) = uparse(str; unit_context = [Unitful, UnitfulAtomic])
 
 Base.show(io::IO, ::SelfConsistentField) = print(io, "scf calculation")
@@ -32,6 +44,7 @@ Base.show(io::IO, ::StructuralOptimization) = print(io, "relax calculation")
 Base.show(io::IO, ::VariableCellOptimization) = print(io, "vc-relax calculation")
 Base.show(io::IO, ::MolecularDynamics) = print(io, "md calculation")
 Base.show(io::IO, ::VariableCellMolecularDynamics) = print(io, "vc-md calculation")
+Base.show(io::IO, ::Step{S,T}) where {S,T} = print(io, "step: $(T()) @ $(S())")
 
 include("Jobs.jl")
 # include("SelfConsistentField.jl")
