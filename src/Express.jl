81bf497e9d8bbf4ef0029b59c114d54ffae4a872
singularitti <singularitti@outlook.com>
2020-10-16 16:37:12 -0400

    Deprecate src/Jobs.jl
    will be replaced by SimpleWorkflow.jl

--- src/Express.jl
+++ src/Express.jl
@@ -5,7 +5,6 @@ abstract type ElectronicStructure <: Calculation end
 abstract type Optimization <: Calculation end
 abstract type VibrationalProperty <: Calculation end
 
-include("Jobs.jl")
 # include("SelfConsistentField.jl")
 # include("BandStructure.jl")
 include("EosFitting/EosFitting.jl")
