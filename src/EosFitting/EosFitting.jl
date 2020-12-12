d8dfe95fab0534a9459db1f35002e6d24d6d9d2e
singularitti <singularitti@outlook.com>
2020-12-11 19:56:02 -0500

    Separate `FitEos` into src/EosFitting/fiteos.jl

--- src/EosFitting/EosFitting.jl
+++ src/EosFitting/EosFitting.jl
@@ -85,33 +85,7 @@ end
 
 const getdata = GetData
 
-struct FitEos{T} <: Action{T} end
-FitEos(::T) where {T<:Calculation} = FitEos{T}()
-function (x::FitEos{T})(
-    data::AbstractVector{<:Pair},
-    trial_eos::EnergyEOS,
-) where {T<:ScfOrOptim}
-    return eosfit(trial_eos, first.(data), last.(data))
-end
-function (x::FitEos{T})(outputs, trial_eos::EnergyEOS) where {T<:ScfOrOptim}
-    data = GetData(T())(outputs)
-    if length(data) <= 5
-        @info "pressures <= 5 may give unreliable results, run more if possible!"
-    end
-    return x(data, trial_eos)
-end
-function (x::FitEos{T})(cfgfile) where {T<:ScfOrOptim}
-    settings = load_settings(cfgfile)
-    outfiles = last.(iofiles(T(), cfgfile))
-    rawsettings = load(cfgfile)
-    saveto = rawsettings["save"]
-    trial_eos = T == SelfConsistentField ? settings.trial_eos : deserialize(saveto)
-    eos = x(outfiles, EnergyEOS(settings.trial_eos))
-    serialize(saveto, eos)
-    return eos
-end
-
-const fiteos = FitEos
+include("fiteos.jl")
 
 struct SaveEos{T} <: Action{T} end
 SaveEos(::T) where {T<:Calculation} = SaveEos{T}()
