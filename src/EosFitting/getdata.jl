755868b5daa208121c2f970d06233ad42015cd96
singularitti <singularitti@outlook.com>
2020-12-11 23:18:36 -0500

    Rewrite `GetData` & deprecate `getdata`

--- src/EosFitting/getdata.jl
+++ src/EosFitting/getdata.jl
@@ -1,9 +1,7 @@
 struct GetData{T} <: Action{T} end
-GetData(::T) where {T<:Calculation} = GetData{T}()
 function (::GetData{T})(outputs) where {T<:ScfOrOptim}
     raw = (load(parseoutput(T()), output) for output in outputs)  # `ntuple` cannot work with generators
     return collect(Iterators.filter(!isnothing, raw))  # A vector of pairs
 end
-const getdata = GetData
 
 function parseoutput end
