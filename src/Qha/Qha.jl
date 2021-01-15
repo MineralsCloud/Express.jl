7989dc2436a9105fd0000f9ad83e087605ea78b2
singularitti <singularitti@outlook.com>
2021-01-15 06:14:40 -0500

    Implement `buildworkflow`

--- src/Qha/Qha.jl
+++ src/Qha/Qha.jl
@@ -1,14 +1,25 @@
 module Qha
 
 using AbInitioSoftwareBase: save, load
+using SimpleWorkflow: chain
 
 using ..Express: Calculation
 
-export QuasiHarmonicApprox, MakeInput, CalculateThermodyn, Plot
+export QuasiHarmonicApprox, MakeInput, CalculateThermodyn, Plot, buildworkflow
 
 struct QuasiHarmonicApprox <: Calculation end
 
 include("config.jl")
+
+function buildjob end
+
+function buildworkflow(cfgfile)
+    return chain(
+        buildjob(MakeInput{QuasiHarmonicApprox}(), cfgfile),
+        buildjob(CalculateThermodyn{QuasiHarmonicApprox}(), cfgfile),
+        buildjob(Plot{QuasiHarmonicApprox}(), cfgfile),
+    )
+end
 
 module DefaultActions
 
@@ -18,6 +29,7 @@ using SimpleWorkflow: InternalAtomicJob
 
 using ...Express: Action, loadconfig
 using ..Qha: QuasiHarmonicApprox, materialize
+import ..Qha: buildjob
 
 include("MakeInput.jl")
 include("Calculate.jl")
