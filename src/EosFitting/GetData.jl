3c6d68fb3f45963bf18cd91dd91767f5814dce4a
singularitti <singularitti@outlook.com>
2021-01-27 23:30:16 -0500

    Fix: use `parseoutput` instead of `load`

--- src/EosFitting/GetData.jl
+++ src/EosFitting/GetData.jl
@@ -1,6 +1,6 @@
 struct GetData{T} <: Action{T} end
 function (::GetData{T})(outputs) where {T<:ScfOrOptim}
-    raw = (load(parseoutput(T()), output) for output in outputs)  # `ntuple` cannot work with generators
+    raw = (parseoutput(T())(output) for output in outputs)  # `ntuple` cannot work with generators
     return collect(Iterators.filter(!isnothing, raw))  # A vector of pairs
 end
 function (x::GetData{T})(file, outputs) where {T}
