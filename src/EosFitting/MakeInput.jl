f7392298ade7d93090e0f54e595ae7269c8ce12f
singularitti <singularitti@outlook.com>
2021-02-07 11:37:36 -0500

    Remove `MakeInput` for `cfgfile`

--- src/EosFitting/MakeInput.jl
+++ src/EosFitting/MakeInput.jl
@@ -5,18 +5,5 @@ function (x::MakeInput)(file, template::Input, args...)
     writetxt(file, input)
     return input
 end
-function (x::MakeInput{T})(cfgfile; kwargs...) where {T}
-    config = loadconfig(cfgfile)
-    infiles = first.(iofiles(T(), cfgfile))
-    eos = PressureEquation(T <: Scf ? config.trial_eos : FitEos{Scf}()(cfgfile))
-    return broadcast(
-        x,
-        infiles,
-        config.templates,
-        config.pressures,
-        fill(eos, length(infiles));
-        kwargs...,
-    )
-end
 
 buildjob(x::MakeInput, cfgfile) = InternalAtomicJob(() -> x(cfgfile))
