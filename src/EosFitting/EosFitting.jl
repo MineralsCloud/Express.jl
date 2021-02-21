ad76b37106085e2f1e17beb412d45b65c0c16268
singularitti <singularitti@outlook.com>
2021-02-21 06:36:30 -0500

    Update `EosFitting.buildworkflow`

--- src/EosFitting/EosFitting.jl
+++ src/EosFitting/EosFitting.jl
@@ -26,6 +26,7 @@ export SelfConsistentField,
     GetData,
     SaveEos,
     MakeCmd,
+    LogMsg,
     loadconfig,
     iofiles,
     calculation,
@@ -53,13 +54,18 @@ end
 function buildjob end
 
 function buildworkflow(cfgfile)
+    step0 = buildjob(LogMsg{Scf}(), true)
     step1 = buildjob(MakeInput{Scf}(), cfgfile)
+    step1 = chain(step0, step1)
     step12 = chain(step1, buildjob(MakeCmd{Scf}(), cfgfile)[1])
     step123 = chain(step12[end], buildjob(FitEos{Scf}(), cfgfile))
     step3p =
         buildjob(GetData{Scf}(), shortname(Scf) * ".json", last.(iofiles(Scf(), cfgfile)))
+    step3p = chain(step3p, buildjob(LogMsg{Scf}(), false))
     step123 = chain(step123[end], step3p)
+    step4m1 = buildjob(LogMsg{VcOptim}(), true)
     step4 = buildjob(MakeInput{VcOptim}(), cfgfile)
+    step4 = chain(step4m1, step4)
     step45 = chain(step4, buildjob(MakeCmd{VcOptim}(), cfgfile)[1])
     step456 = chain(step45[end], buildjob(FitEos{VcOptim}(), cfgfile))
     step6p = buildjob(
@@ -67,6 +73,7 @@ function buildworkflow(cfgfile)
         shortname(VcOptim) * ".json",
         last.(iofiles(VcOptim(), cfgfile)),
     )
+    step6p = chain(step6p, buildjob(LogMsg{VcOptim}(), false))
     step456 = chain(step456[end], step6p)
     step16 = chain(step123[end], step456[1])
     return step16
