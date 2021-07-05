a98f604cad2afdc66bc3e43d58cbb2cdabbf7011
Reno <25192197+singularitti@users.noreply.github.com>
2021-07-05 17:38:47 -0400

    Remove the use of LegibleLambdas.jl

--- src/EosFitting/EosFitting.jl
+++ src/EosFitting/EosFitting.jl
@@ -97,7 +97,6 @@ using Dates: now, format
 using EquationsOfStateOfSolids:
     EquationOfStateOfSolids, EnergyEquation, PressureEquation, Parameters, getparam
 using EquationsOfStateOfSolids.Fitting: eosfit
-using LegibleLambdas: @λ
 using Logging: with_logger, current_logger
 using Serialization: serialize, deserialize
 using SimpleWorkflow: InternalAtomicJob
@@ -116,7 +115,7 @@ include("SaveEos.jl")
 include("LogMsg.jl")
 
 buildjob(x::Union{MakeInput,GetData,FitEos,LogMsg}, args...) =
-    InternalAtomicJob(@λ(() -> x(args...)))
+    InternalAtomicJob(() -> x(args...))
 
 end
 
