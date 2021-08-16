3bae880398f5b8d2930d6a83941e9b65604e4312
Reno <25192197+singularitti@users.noreply.github.com>
2021-08-16 01:57:08 -0400

    Move things to src/EquationOfStateWorkflow/DefaultActions.jl

--- src/EquationOfStateWorkflow/MakeInput.jl
+++
@@ -1,7 +0,0 @@
-struct MakeInput{T} <: Action{T} end
-function (x::MakeInput)(file, template::Input, args...)
-    input = x(template, args...)
-    mkpath(dirname(file))  # In case its parent directory is not created
-    writetxt(file, input)
-    return input
-end
