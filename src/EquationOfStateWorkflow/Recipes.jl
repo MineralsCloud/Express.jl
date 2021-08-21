dce116fd477ec37453d40b9e531174e5302c1fb6
Reno <25192197+singularitti@users.noreply.github.com>
2021-08-21 04:31:50 -0400

    Rewrite `buildworkflow`

--- src/EquationOfStateWorkflow/Recipes.jl
+++ src/EquationOfStateWorkflow/Recipes.jl
@@ -1,7 +1,7 @@
 module Recipes
 
 using Serialization: deserialize
-using SimpleWorkflows: AtomicJob, Workflow, run!, ▷
+using SimpleWorkflows: AtomicJob, Workflow, run!, ▷, ⋲, ⋺
 
 using ...Config: loadconfig
 using ..DefaultActions: LogMsg, MakeInput, RunCmd, GetData, FitEos
@@ -17,20 +17,31 @@ function buildworkflow(cfgfile)
         return w
     else
         a = AtomicJob(() -> LogMsg{Scf}()(true))
-        b = AtomicJob(() -> MakeInput{Scf}()(cfgfile))
-        c = AtomicJob(() -> RunCmd{Scf}()(cfgfile))
+        b = map(x -> AtomicJob(() -> x), MakeInput{Scf}()(cfgfile))
+        c = map(x -> AtomicJob(() -> x), RunCmd{Scf}()(cfgfile))
         d = AtomicJob(() -> FitEos{Scf}()(cfgfile))
         e = AtomicJob(() -> GetData{Scf}()(string(Scf) * ".json", last.(config.files)))
         f = AtomicJob(() -> LogMsg{Scf}()(false))
         g = AtomicJob(() -> LogMsg{VcOptim}()(true))
-        h = AtomicJob(() -> MakeInput{VcOptim}()(cfgfile))
-        i = AtomicJob(() -> RunCmd{VcOptim}()(cfgfile))
+        h = map(x -> AtomicJob(() -> x), MakeInput{VcOptim}()(cfgfile))
+        i = map(x -> AtomicJob(() -> x), RunCmd{VcOptim}()(cfgfile))
         j = AtomicJob(() -> FitEos{VcOptim}()(cfgfile))
         k = AtomicJob(
             () -> GetData{VcOptim}()(string(VcOptim) * ".json", last.(config.files)),
         )
         l = AtomicJob(() -> LogMsg{VcOptim}()(false))
-        a ▷ b ▷ c ▷ d ▷ e ▷ f ▷ g ▷ h ▷ i ▷ j ▷ k ▷ l
+        ⋲(a, b...)
+        for (x, y) in zip(b, c)
+            x ▷ y
+        end
+        ⋺(c..., d)
+        d ▷ e ▷ f ▷ g
+        ⋲(g, h...)
+        for (x, y) in zip(h, i)
+            x ▷ y
+        end
+        ⋺(i..., j)
+        j ▷ k ▷ l
         return Workflow(a, b, c, d, e, f, g, h, i, j, k, l)
     end
 end
