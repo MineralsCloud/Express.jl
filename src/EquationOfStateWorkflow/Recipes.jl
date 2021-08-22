970048c47b4c4e91a78d0f6afb2951e33b30b07e
Reno <25192197+singularitti@users.noreply.github.com>
2021-08-22 01:25:02 -0400

    Fix `buildworkflow` using `load` & `ExpandConfig`

--- src/EquationOfStateWorkflow/Recipes.jl
+++ src/EquationOfStateWorkflow/Recipes.jl
@@ -1,18 +1,19 @@
 module Recipes
 
+using AbInitioSoftwareBase: load
 using Serialization: deserialize
 using SimpleWorkflows: AtomicJob, Workflow, run!, ▷, ⋲, ⋺
 
-using ...Config: loadconfig
+using ..Config: ExpandConfig
 using ..DefaultActions: LogMsg, MakeInput, RunCmd, GetData, FitEos, buildjob
 using ..EquationOfStateWorkflow: Scf, VcOptim
 
 export buildworkflow
 
 function buildworkflow(cfgfile)
-    config = loadconfig(cfgfile)
-    if isfile(config.recover)
-        w = deserialize(config.recover)
+    dict = load(cfgfile)
+    if isfile(dict["recover"])
+        w = deserialize(dict["recover"])
         typeassert(w, Workflow)
         return w
     else
@@ -20,15 +21,13 @@ function buildworkflow(cfgfile)
         b = buildjob(MakeInput{Scf}(), cfgfile)
         c = buildjob(RunCmd{Scf}(), cfgfile)
         d = buildjob(FitEos{Scf}(), cfgfile)
-        e = AtomicJob(() -> GetData{Scf}()(string(Scf) * ".json", last.(config.files)))
+        e = buildjob(GetData{Scf}(), cfgfile)
         f = AtomicJob(() -> LogMsg{Scf}()(false))
         g = AtomicJob(() -> LogMsg{VcOptim}()(true))
         h = buildjob(MakeInput{VcOptim}(), cfgfile)
         i = buildjob(RunCmd{VcOptim}(), cfgfile)
         j = buildjob(FitEos{Scf}(), cfgfile)
-        k = AtomicJob(
-            () -> GetData{VcOptim}()(string(VcOptim) * ".json", last.(config.files)),
-        )
+        k = buildjob(GetData{VcOptim}(), cfgfile)
         l = AtomicJob(() -> LogMsg{VcOptim}()(false))
         ⋲(a, b...)
         for (x, y) in zip(b, c)
