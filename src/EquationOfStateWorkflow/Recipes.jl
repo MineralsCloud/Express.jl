31de9a02a7170faf8db8e265a4c41e382f934f22
Reno <25192197+singularitti@users.noreply.github.com>
2021-08-22 02:13:26 -0400

    Remove unused `GetData` in `buildworkflow`

--- src/EquationOfStateWorkflow/Recipes.jl
+++ src/EquationOfStateWorkflow/Recipes.jl
@@ -21,27 +21,25 @@ function buildworkflow(cfgfile)
         b = buildjob(MakeInput{Scf}(), cfgfile)
         c = buildjob(RunCmd{Scf}(), cfgfile)
         d = buildjob(FitEos{Scf}(), cfgfile)
-        e = buildjob(GetData{Scf}(), cfgfile)
         f = AtomicJob(() -> LogMsg{Scf}()(false))
         g = AtomicJob(() -> LogMsg{VcOptim}()(true))
         h = buildjob(MakeInput{VcOptim}(), cfgfile)
         i = buildjob(RunCmd{VcOptim}(), cfgfile)
         j = buildjob(FitEos{Scf}(), cfgfile)
-        k = buildjob(GetData{VcOptim}(), cfgfile)
         l = AtomicJob(() -> LogMsg{VcOptim}()(false))
         ⋲(a, b...)
         for (x, y) in zip(b, c)
             x ▷ y
         end
         ⋺(c..., d)
-        d ▷ e ▷ f ▷ g
+        d ▷ f ▷ g
         ⋲(g, h...)
         for (x, y) in zip(h, i)
             x ▷ y
         end
         ⋺(i..., j)
-        j ▷ k ▷ l
-        return Workflow(a, b, c, d, e, f, g, h, i, j, k, l)
+        j ▷ l
+        return Workflow(a, b, c, d, f, g, h, i, j, l)
     end
 end
 
