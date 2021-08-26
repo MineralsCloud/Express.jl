cba3c09a8398a1c9451278e24261487c6752c5e4
Reno <25192197+singularitti@users.noreply.github.com>
2021-08-26 04:53:12 -0400

    Move `buildworkflow` to src/PhononWorkflow/Recipes.jl

--- src/PhononWorkflow/PhononWorkflow.jl
+++ src/PhononWorkflow/PhononWorkflow.jl
@@ -47,43 +47,6 @@ const Dfpt = LinearResponse
 const VDos = PhononDensityOfStates
 const ZoneCentrePhonons = ZoneCenterPhonons  # For English users
 
-function buildjob end
-
-function buildworkflow(cfgfile)
-    config = loadconfig(cfgfile)
-    if isfile(config.recover)
-        w = deserialize(config.recover)
-        typeassert(w, Workflow)
-        return w
-    else
-        settings = load(cfgfile)
-        x = if settings["workflow"] == "phonon dispersion"
-            PhononDispersion
-        elseif settings["workflow"] == "vdos"
-            VDos
-        else
-            error("unsupported option!")
-        end
-        return begin
-            @intjob(LogMsg{Scf}()(true)) ▷
-            @intjob(MakeInput{Scf}()(cfgfile)) ▷
-            buildjob(MakeCmd{Scf}(), cfgfile) ▷
-            @intjob(LogMsg{Scf}()(false)) ▷
-            @intjob(LogMsg{Dfpt}()(true)) ▷
-            @intjob(MakeInput{Dfpt}()(cfgfile)) ▷
-            buildjob(MakeCmd{Dfpt}(), cfgfile) ▷
-            @intjob(LogMsg{Dfpt}()(false)) ▷
-            @intjob(LogMsg{RealSpaceForceConstants}()(true)) ▷
-            @intjob(MakeInput{RealSpaceForceConstants}()(cfgfile)) ▷
-            buildjob(MakeCmd{RealSpaceForceConstants}(), cfgfile) ▷
-            @intjob(LogMsg{RealSpaceForceConstants}()(false)) ▷
-            @intjob(LogMsg{x}()(true)) ▷
-            @intjob(MakeInput{x}()(cfgfile)) ▷
-            buildjob(MakeCmd{x}(), cfgfile) ▷ @intjob(LogMsg{x}()(false))
-        end
-    end
-end
-
 order(x) = order(typeof(x))
 order(::Type{Scf}) = 1
 order(::Type{Dfpt}) = 2
--- src/PhononWorkflow/Recipes.jl
+++ src/PhononWorkflow/Recipes.jl
@@ -1,3 +1,33 @@
 module Recipes
 
+function buildworkflow(cfgfile)
+    config = loadconfig(cfgfile)
+    if isfile(config.recover)
+        w = deserialize(config.recover)
+        typeassert(w, Workflow)
+        return w
+    else
+        settings = load(cfgfile)
+        x = if settings["workflow"] == "phonon dispersion"
+            PhononDispersion
+        elseif settings["workflow"] == "vdos"
+            VDos
+        else
+            error("unsupported option!")
+        end
+        return begin
+            @intjob(LogMsg{Scf}()(true)) ▷ @intjob(MakeInput{Scf}()(cfgfile)) ▷
+            buildjob(MakeCmd{Scf}(), cfgfile) ▷ @intjob(LogMsg{Scf}()(false)) ▷
+            @intjob(LogMsg{Dfpt}()(true)) ▷ @intjob(MakeInput{Dfpt}()(cfgfile)) ▷
+            buildjob(MakeCmd{Dfpt}(), cfgfile) ▷ @intjob(LogMsg{Dfpt}()(false)) ▷
+            @intjob(LogMsg{RealSpaceForceConstants}()(true)) ▷
+            @intjob(MakeInput{RealSpaceForceConstants}()(cfgfile)) ▷
+            buildjob(MakeCmd{RealSpaceForceConstants}(), cfgfile) ▷
+            @intjob(LogMsg{RealSpaceForceConstants}()(false)) ▷ @intjob(LogMsg{x}()(true)) ▷
+            @intjob(MakeInput{x}()(cfgfile)) ▷ buildjob(MakeCmd{x}(), cfgfile) ▷
+            @intjob(LogMsg{x}()(false))
+        end
+    end
+end
+
 end
