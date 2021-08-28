db2dfae167139f0a7161a3ee47bb0fe591a0c112
Reno <25192197+singularitti@users.noreply.github.com>
2021-08-28 18:17:29 -0400

    Include QuasiHarmonicApproxWorkflow/QuasiHarmonicApproxWorkflow.jl

--- src/Express.jl
+++ src/Express.jl
@@ -58,6 +58,6 @@ include("Shell.jl")
 using .Shell
 include("EquationOfStateWorkflow/EquationOfStateWorkflow.jl")
 include("PhononWorkflow/PhononWorkflow.jl")
-# include("QuasiHarmonicApproxWorkflow/QuasiHarmonicApproxWorkflow.jl")
+include("QuasiHarmonicApproxWorkflow/QuasiHarmonicApproxWorkflow.jl")
 
 end
