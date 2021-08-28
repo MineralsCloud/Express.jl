c64a897c9386aa5cb01ee884b3fe3bc57025bca5
Reno <25192197+singularitti@users.noreply.github.com>
2021-08-28 18:17:08 -0400

    Move `buildworkflow` to src/QuasiHarmonicApproxWorkflow/Recipes.jl

--- src/QuasiHarmonicApproxWorkflow/QuasiHarmonicApproxWorkflow.jl
+++ src/QuasiHarmonicApproxWorkflow/QuasiHarmonicApproxWorkflow.jl
@@ -3,21 +3,11 @@ module QuasiHarmonicApproxWorkflow
 using AbInitioSoftwareBase: save, load
 using ..Express: Calculation
 
-export QuasiHarmonicApprox, MakeInput, CalculateThermodyn, Plot, buildworkflow
+export QuasiHarmonicApprox, MakeInput, CalculateThermodyn, Plot
 
 struct QuasiHarmonicApprox <: Calculation end
 
 include("Config.jl")
-
-function buildjob end
-
-function buildworkflow(cfgfile)
-    return chain(
-        buildjob(MakeInput{QuasiHarmonicApprox}(), cfgfile),
-        buildjob(CalculateThermodyn{QuasiHarmonicApprox}(), cfgfile),
-        buildjob(Plot{QuasiHarmonicApprox}(), cfgfile),
-    )
-end
 
 include("DefaultActions.jl")
 using .DefaultActions: MakeInput, CalculateThermodyn, Plot
--- src/QuasiHarmonicApproxWorkflow/Recipes.jl
+++ src/QuasiHarmonicApproxWorkflow/Recipes.jl
@@ -1,3 +1,11 @@
 module Recipes
 
+using SimpleWorkflows: AtomicJob, Workflow, run!, ▷
+
+function buildworkflow(cfgfile)
+    return buildjob(MakeInput{QuasiHarmonicApprox}(), cfgfile) ▷
+           buildjob(CalculateThermodyn{QuasiHarmonicApprox}(), cfgfile) ▷
+           buildjob(Plot{QuasiHarmonicApprox}(), cfgfile)
+end
+
 end
