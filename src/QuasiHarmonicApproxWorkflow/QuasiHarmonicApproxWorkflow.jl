cff53cdb8037b0c759f413b9878e26fc15ceb1d3
Reno <25192197+singularitti@users.noreply.github.com>
2021-09-25 19:37:09 -0400

    Deprecate `DefaultActions` as module

--- src/QuasiHarmonicApproxWorkflow/QuasiHarmonicApproxWorkflow.jl
+++ src/QuasiHarmonicApproxWorkflow/QuasiHarmonicApproxWorkflow.jl
@@ -8,10 +8,7 @@ export QuasiHarmonicApprox, MakeInput, CalculateThermodyn, Plot
 struct QuasiHarmonicApprox <: Calculation end
 
 include("Config.jl")
-
 include("DefaultActions.jl")
-using .DefaultActions: MakeInput, CalculateThermodyn, Plot
-
 include("Recipes.jl")
 
 end
