86a473ee211ddee83c0c7e5e9f9ea683f3ce7259
Reno <25192197+singularitti@users.noreply.github.com>
2021-09-27 04:20:21 -0400

    Rename `buildworkflow` to `build` for `Recipe`

--- src/EquationOfStateWorkflow/Recipes.jl
+++ src/EquationOfStateWorkflow/Recipes.jl
@@ -8,28 +8,28 @@ using ..Config: ExpandConfig
 using ..EquationOfStateWorkflow:
     Scf, VcOptim, LogMsg, MakeInput, RunCmd, GetData, FitEos, buildjob
 
-export buildworkflow, run!
+export build, run!
 
 struct Recipe{T}
     config::Dict{String,Any}
 end
 
-function buildworkflow(cfgfile)
-    dict = load(cfgfile)
+function build(recipe::Recipe{:eos})
+    dict = load(recipe.config)
     if isfile(dict["save"]["status"])
         w = deserialize(dict["save"]["status"])
         typeassert(w, Workflow)
         return w
     else
         a = AtomicJob(() -> LogMsg{Scf}()(; start = true))
-        b = buildjob(MakeInput{Scf}(), cfgfile)
-        c = buildjob(RunCmd{Scf}(), cfgfile)
-        d = buildjob(FitEos{Scf}(), cfgfile)
+        b = buildjob(MakeInput{Scf}(), recipe.config)
+        c = buildjob(RunCmd{Scf}(), recipe.config)
+        d = buildjob(FitEos{Scf}(), recipe.config)
         f = AtomicJob(() -> LogMsg{Scf}()(; start = false))
         g = AtomicJob(() -> LogMsg{VcOptim}()(; start = true))
-        h = buildjob(MakeInput{VcOptim}(), cfgfile)
-        i = buildjob(RunCmd{VcOptim}(), cfgfile)
-        j = buildjob(FitEos{VcOptim}(), cfgfile)
+        h = buildjob(MakeInput{VcOptim}(), recipe.config)
+        i = buildjob(RunCmd{VcOptim}(), recipe.config)
+        j = buildjob(FitEos{VcOptim}(), recipe.config)
         l = AtomicJob(() -> LogMsg{VcOptim}()(; start = false))
         (((((((a ⋲ b) ▷ c) ⋺ d) ▷ f) ▷ g ⋲ h) ▷ i) ⋺ j) ▷ l
         return Workflow(a, b..., c..., d, f, g, h..., i..., j, l)
