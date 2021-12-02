fd6d705982d0a1b36656e01953469075336a7d08
Reno <25192197+singularitti@users.noreply.github.com>
2021-12-02 05:33:14 -0500

    Add type `KMesh`

--- src/ConvergenceTestWorkflow/Config.jl
+++ src/ConvergenceTestWorkflow/Config.jl
@@ -9,6 +9,14 @@ using ...Express: Calculation, Action, UnitfulVector, myuparse
     unit::String = "Ry"
 end
 
-
+@option "k_mesh" struct KMesh <: UnitfulVector
+    mesh::AbstractVector
+    is_shift::AbstractVector = [0, 0, 0]
+    function KMesh(mesh, is_shift)
+        @assert all(mesh .>= 1)
+        @assert all(0 <= x <= 1 for x in is_shift)
+        return new(mesh, is_shift)
+    end
+end
 
 end
