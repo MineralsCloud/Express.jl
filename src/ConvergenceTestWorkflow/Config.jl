2392bc6edcf24d3582629e422cb681b8d7bea399
Reno <25192197+singularitti@users.noreply.github.com>
2021-12-02 05:42:03 -0500

    Add types `Directories`, `FileNamePatterns`, `Save`, `IOFiles`, & `RuntimeConfig`

--- src/ConvergenceTestWorkflow/Config.jl
+++ src/ConvergenceTestWorkflow/Config.jl
@@ -1,5 +1,6 @@
 module Config
 
+using AbInitioSoftwareBase.Commands: CommandConfig
 using Configurations: from_dict, @option
 
 using ...Express: Calculation, Action, UnitfulVector, myuparse
@@ -16,6 +17,41 @@ end
         @assert all(mesh .>= 1)
         @assert all(0 <= x <= 1 for x in is_shift)
         return new(mesh, is_shift)
+    end
+end
+
+@option struct Directories
+    root::String = pwd()
+end
+
+@option struct FileNamePatterns
+    input::String = "%s.in"
+    output::String = "%s.out"
+end
+
+@option struct Save
+    raw::String = "raw.json"
+    status::String = ""
+end
+
+@option struct IOFiles
+    dirs::Directories = Directories()
+    pattern::FileNamePatterns = FileNamePatterns()
+end
+
+@option struct RuntimeConfig
+    recipe::String
+    template::String
+    parameters::Union{CutoffEnergy,KMesh}
+    files::IOFiles = IOFiles()
+    save::Save = Save()
+    cli::CommandConfig
+    function RuntimeConfig(recipe, template, parameters, files, save, cli)
+        @assert recipe in ("ecut", "k_mesh")
+        if !isfile(template)
+            @warn "I cannot find template file `$template`!"
+        end
+        return new(recipe, template, parameters, files, save, cli)
     end
 end
 
