80323631d9cd9ad0fa23a63f3dac26d00c80351f
Reno <25192197+singularitti@users.noreply.github.com>
2022-01-16 01:51:10 -0500

    Reformat code in src/ConvergenceTestWorkflow/actions.jl

--- src/ConvergenceTestWorkflow/actions.jl
+++ src/ConvergenceTestWorkflow/actions.jl
@@ -88,14 +88,12 @@ struct TestConvergence{T} <: Action{T} end
 function buildjob(x::TestConvergence{T}, cfgfile) where {T}
     dict = load(cfgfile)
     config = ExpandConfig{T}()(dict)
-    return AtomicJob(
-        function ()
-            data = GetData{T}()(last.(config.files))
-            saved = Dict("results" => (ustrip ∘ last).(data))
-            save(config.save_raw, saved)
-            return x(data)
-        end,
-    )
+    return AtomicJob(function ()
+        data = GetData{T}()(last.(config.files))
+        saved = Dict("results" => (ustrip ∘ last).(data))
+        save(config.save_raw, saved)
+        return x(data)
+    end)
 end
 
 struct LogMsg{T} <: Action{T} end
