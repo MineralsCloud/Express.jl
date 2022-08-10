a8ab8dfc741ef7315769b95a2c575f23d1a78c82
Reno <singularitti@outlook.com>
2022-08-10 19:02:34 -0400

    Add `Calculation` & subtypes & `calculation` method

--- ExpressBase/src/ExpressBase.jl
+++ ExpressBase/src/ExpressBase.jl
@@ -1,5 +1,16 @@
 module ExpressBase
 
-# Write your package code here.
+abstract type Calculation end
+abstract type ElectronicStructure <: Calculation end
+struct SelfConsistentField <: ElectronicStructure end
+abstract type Optimization <: Calculation end
+abstract type LatticeDynamics <: Calculation end
+# Aliases
+const Scf = SelfConsistentField
+const FixedIonSelfConsistentField = SelfConsistentField
+
+abstract type Action{T<:Calculation} end
+
+calculation(::Action{T}) where {T} = T()
 
 end
