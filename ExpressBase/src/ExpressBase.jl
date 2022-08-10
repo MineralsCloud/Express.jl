49e3d6214677ae87037f7f8684e8f2044a580bc9
Reno <singularitti@outlook.com>
2022-08-10 19:26:52 -0400

    Update types in src/ExpressBase.jl

--- ExpressBase/src/ExpressBase.jl
+++ ExpressBase/src/ExpressBase.jl
@@ -1,13 +1,53 @@
 module ExpressBase
 
+export SelfConsistentField,
+    NonSelfConsistentField,
+    BandStructure,
+    DftPlusU,
+    FixedCellOptimization,
+    VariableCellOptimization,
+    DensityFunctionalPerturbationTheory,
+    RealSpaceForceConstants,
+    PhononDispersion,
+    PhononDensityOfStates,
+    ZoneCenterPhonons,
+    QuasiHarmonicApproximation,
+    Scf,
+    NScf,
+    FixedIonSelfConsistentField,
+    Dfpt,
+    VDos,
+    ZoneCentrePhonons,
+    Qha
+export calculation
+
+# See https://www.quantum-espresso.org/Doc/pw_user_guide/node10.html
 abstract type Calculation end
 abstract type ElectronicStructure <: Calculation end
 struct SelfConsistentField <: ElectronicStructure end
+struct NonSelfConsistentField <: ElectronicStructure end
+struct BandStructure <: ElectronicStructure end
+struct DftPlusU <: ElectronicStructure end
+# Optimization
 abstract type Optimization <: Calculation end
+struct FixedCellOptimization <: Optimization end
+struct VariableCellOptimization <: Optimization end
+# Phonon
 abstract type LatticeDynamics <: Calculation end
+struct DensityFunctionalPerturbationTheory <: LatticeDynamics end
+struct RealSpaceForceConstants <: LatticeDynamics end
+struct PhononDispersion <: LatticeDynamics end
+struct PhononDensityOfStates <: LatticeDynamics end
+struct ZoneCenterPhonons <: LatticeDynamics end
+struct QuasiHarmonicApproximation <: Calculation end
 # Aliases
 const Scf = SelfConsistentField
+const NScf = NonSelfConsistentField
 const FixedIonSelfConsistentField = SelfConsistentField
+const Dfpt = DensityFunctionalPerturbationTheory
+const VDos = PhononDensityOfStates
+const ZoneCentrePhonons = ZoneCenterPhonons  # For British users
+const Qha = QuasiHarmonicApproximation
 
 abstract type Action{T<:Calculation} end
 
