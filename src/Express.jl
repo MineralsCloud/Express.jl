a4729fa2f753328e7cb88b32b1737058fa806259
Reno <singularitti@outlook.com>
2022-08-12 08:59:20 -0400

    Deprecate `myuparse`, `convert_to_option` & `UnitfulVector` in src/Express.jl

--- src/Express.jl
+++ src/Express.jl
@@ -1,20 +1,6 @@
 module Express
 
-using AbInitioSoftwareBase: load
-using AbInitioSoftwareBase.Inputs: Input
-using Unitful: Unitful, uparse, unitmodules
-
-import Configurations: convert_to_option
-
-myuparse(str::AbstractString) =
-    uparse(filter(!isspace, str); unit_context = push!(unitmodules, Unitful))
-
 function current_software end
-
-abstract type UnitfulVector end
-
-convert_to_option(::Type{<:UnitfulVector}, ::Type{AbstractVector}, str::AbstractString) =
-    eval(Meta.parse(str))
 
 # include("SelfConsistentField.jl")
 # include("BandStructure.jl")
