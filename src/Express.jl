3e5ff334e9d24ea41f53d32d952bb7aca6995a40
Reno <singularitti@outlook.com>
2022-08-13 22:43:28 -0400

    Deprecate Shell.jl & `distprocs`

--- src/Express.jl
+++ src/Express.jl
@@ -4,8 +4,6 @@ function current_software end
 
 # include("SelfConsistentField.jl")
 # include("BandStructure.jl")
-include("Shell.jl")
-using .Shell
 include("ConvergenceTestWorkflow/ConvergenceTestWorkflow.jl")
 include("EquationOfStateWorkflow/EquationOfStateWorkflow.jl")
 include("PhononWorkflow/PhononWorkflow.jl")
