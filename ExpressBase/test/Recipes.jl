f1c94205d306dd3a47ee29f1a01f40257aebcd40
Reno <singularitti@outlook.com>
2022-08-14 17:30:06 -0400

    Update "Test `==` of `ComposedRecipe`" & "Test `⨟` of `ComposedRecipe`"

--- ExpressBase/test/Recipes.jl
+++ ExpressBase/test/Recipes.jl
@@ -18,17 +18,19 @@ const d = RecipeD()
     @test a ∘ b == a ∘ b == ∘(a, b)
     @test c ∘ d != d ∘ c != c != d
     @test a ∘ c != a ∘ b != b ∘ c
-    @test a ∘ b ∘ c == ∘(a, b, c)
-    @test a ∘ b ∘ c ∘ d == ∘(a, b, c, d)
+    @test a ∘ b ∘ c == ∘(a, b, c) == (a ∘ b) ∘ c != a ∘ (b ∘ c)
+    @test a ∘ b ∘ c ∘ d == ∘(a, b, c, d) == ((a ∘ b) ∘ c) ∘ d
+    @test (a ∘ b) ∘ (c ∘ d) != ((a ∘ b) ∘ c) ∘ d != a ∘ (b ∘ c) ∘ d
 end
 
 @testset "Test `⨟` of `ComposedRecipe`" begin
     @test ⨟(a) == a
-    @test a ⨟ b == b ∘ a
-    @test ⨟(a, b, c) == c ∘ b ∘ a == (c ∘ b) ∘ a
+    @test a ⨟ b == ⨟(a, b) == b ∘ a
+    @test ⨟(a, b, c) == (c ∘ b) ∘ a == c ∘ b ∘ a
     @test a ⨟ b ⨟ c == c ∘ (b ∘ a) != c ∘ b ∘ a
-    @test ⨟(a, b, c, d) == d ∘ c ∘ b ∘ a == ((d ∘ c) ∘ b) ∘ a
+    @test ⨟(a, b, c, d) == ((d ∘ c) ∘ b) ∘ a == d ∘ c ∘ b ∘ a
     @test a ⨟ b ⨟ c ⨟ d == d ∘ (c ∘ (b ∘ a)) != d ∘ c ∘ b ∘ a
+    @test (a ⨟ b) ⨟ (c ⨟ d) == (d ∘ c) ∘ (b ∘ a)
 end
 
 end
