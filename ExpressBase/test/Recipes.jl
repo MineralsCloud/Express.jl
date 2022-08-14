22ad0e80d9a17e177e86d38828cfed3a1f46099b
Reno <singularitti@outlook.com>
2022-08-14 17:51:45 -0400

    Add "Test `decompose` of `ComposedRecipe`" & "Test `deopcompose` of `ComposedRecipe`"

--- ExpressBase/test/Recipes.jl
+++ ExpressBase/test/Recipes.jl
@@ -1,6 +1,6 @@
 module Recipes
 
-using CompositionsBase: ⨟
+using CompositionsBase: ⨟, decompose, deopcompose
 using ExpressBase.Recipes: Recipe
 using Test: @testset, @test
 
@@ -31,6 +31,38 @@ end
     @test ⨟(a, b, c, d) == a ⨟ (b ⨟ (c ⨟ d)) == ((d ∘ c) ∘ b) ∘ a == d ∘ c ∘ b ∘ a
     @test a ⨟ b ⨟ c ⨟ d == d ∘ (c ∘ (b ∘ a)) != d ∘ c ∘ b ∘ a
     @test (a ⨟ b) ⨟ (c ⨟ d) == (d ∘ c) ∘ (b ∘ a)
+end
+
+@testset "Test `decompose` of `ComposedRecipe`" begin
+    @test decompose(a) == (a,)
+    @test decompose(a ∘ b) == (a, b)
+    @test decompose(a ∘ b ∘ c) == (a, b, c)
+    @test decompose(a ∘ (b ∘ c)) == (a, b, c)
+    @test decompose(a ∘ b ∘ c ∘ d) == (a, b, c, d)
+    @test decompose(a ∘ (b ∘ (c ∘ d))) == (a, b, c, d)
+    @test decompose((a ∘ b) ∘ (c ∘ d)) == (a, b, c, d)
+    @test decompose(a ⨟ b) == decompose(⨟(a, b)) == (b, a)
+    @test decompose(a ⨟ b ⨟ c) == (c, b, a)
+    @test decompose(⨟(a, b, c)) == decompose(a ⨟ (b ⨟ c)) == (c, b, a)
+    @test decompose(a ⨟ b ⨟ c ⨟ d) == (d, c, b, a)
+    @test decompose(⨟(a, b, c, d)) == decompose(a ⨟ (b ⨟ (c ⨟ d))) == (d, c, b, a)
+    @test decompose((a ⨟ b) ⨟ (c ⨟ d)) == (d, c, b, a)
+end
+
+@testset "Test `deopcompose` of `ComposedRecipe`" begin
+    @test deopcompose(a) == (a,)
+    @test deopcompose(a ∘ b) == (b, a)
+    @test deopcompose(a ∘ b ∘ c) == (c, b, a)
+    @test deopcompose(a ∘ (b ∘ c)) == (c, b, a)
+    @test deopcompose(a ∘ b ∘ c ∘ d) == (d, c, b, a)
+    @test deopcompose(a ∘ (b ∘ (c ∘ d))) == (d, c, b, a)
+    @test deopcompose((a ∘ b) ∘ (c ∘ d)) == (d, c, b, a)
+    @test deopcompose(a ⨟ b) == deopcompose(⨟(a, b)) == (a, b)
+    @test deopcompose(a ⨟ b ⨟ c) == (a, b, c)
+    @test deopcompose(⨟(a, b, c)) == deopcompose(a ⨟ (b ⨟ c)) == (a, b, c)
+    @test deopcompose(a ⨟ b ⨟ c ⨟ d) == (a, b, c, d)
+    @test deopcompose(⨟(a, b, c, d)) == deopcompose(a ⨟ (b ⨟ (c ⨟ d))) == (a, b, c, d)
+    @test deopcompose((a ⨟ b) ⨟ (c ⨟ d)) == (a, b, c, d)
 end
 
 end
