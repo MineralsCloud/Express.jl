66d378133ad7d1713f348d18380366d7463541f1
Reno <singularitti@outlook.com>
2022-08-14 00:21:43 -0400

    Add test/Recipes.jl

---
+++ ExpressBase/test/Recipes.jl
@@ -0,0 +1,30 @@
+module Recipes
+
+using ExpressBase.Recipes: Recipe
+using Test: @testset, @test
+
+struct RecipeA <: Recipe end
+struct RecipeB <: Recipe end
+struct RecipeC <: Recipe end
+struct RecipeD <: Recipe end
+
+const a = RecipeA()
+const b = RecipeB()
+const c = RecipeC()
+const d = RecipeD()
+
+@testset "Test `==` of `ComposedRecipe`" begin
+    @test a ∘ b == a ∘ b == ∘(a, b)
+    @test c ∘ d != d ∘ c != c != d
+    @test a ∘ c != a ∘ b != b ∘ c
+    @test a ∘ b ∘ c == ∘(a, b, c)
+    @test a ∘ b ∘ c ∘ d == ∘(a, b, c, d)
+end
+
+@testset "Test `inv` of `ComposedRecipe`" begin
+    @test inv(a ∘ b) == b ∘ a
+    @test inv(a ∘ b ∘ c) == c ∘ b ∘ a
+    @test inv(a ∘ b ∘ c ∘ d) == d ∘ c ∘ b ∘ a
+end
+
+end
