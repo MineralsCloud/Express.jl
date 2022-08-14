c7abc650a72718c2a4ccc72f569cc1819a0cc799
Reno <singularitti@outlook.com>
2022-08-14 19:49:00 -0400

    Rename `buildworkflow` to `build` from `Recipe`s in src/QuasiHarmonicApproxWorkflow/Recipes.jl

--- src/QuasiHarmonicApproxWorkflow/Recipes.jl
+++ src/QuasiHarmonicApproxWorkflow/Recipes.jl
@@ -1,19 +1,42 @@
 module Recipes
 
+using AbInitioSoftwareBase: load
 using SimpleWorkflows: Job, Workflow, run!, →
 using ExpressBase: QuasiHarmonicApproximation
+using ExpressBase.Recipes: Recipe
 using ExpressWorkflowMaker.Templates: jobify
 
 using ..QuasiHarmonicApproxWorkflow: MakeInput, CalculateThermodyn, Plot
 
-export buildworkflow, run!
+export build, run!
 
-function buildworkflow(cfgfile)
-    a = jobify(MakeInput{QuasiHarmonicApproximation}(), cfgfile)
-    b = jobify(CalculateThermodyn{QuasiHarmonicApproximation}(), cfgfile)
-    c = jobify(Plot{QuasiHarmonicApproximation}(), cfgfile)
+struct SingleConfigurationRecipe <: Recipe
+    config
+end
+struct MultiConfigurationRecipe <: Recipe
+    config
+end
+
+function build(
+    ::Type{Workflow},
+    r::Union{SingleConfigurationRecipe,MultiConfigurationRecipe},
+)
+    a = jobify(MakeInput{QuasiHarmonicApproximation}(), r.config)
+    b = jobify(CalculateThermodyn{QuasiHarmonicApproximation}(), r.config)
+    c = jobify(Plot{QuasiHarmonicApproximation}(), r.config)
     a → b → c
     return Workflow(a)
+end
+function build(::Type{Workflow}, file)
+    dict = load(file)
+    recipe = dict["recipe"]
+    if recipe == "qha single"
+        return build(Workflow, SingleConfigurationRecipe(dict))
+    elseif recipe == "multi qha"
+        return build(Workflow, MultiConfigurationRecipe(dict))
+    else
+        error("unsupported recipe $recipe.")
+    end
 end
 
 end
