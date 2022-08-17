b13e6ab01187c3d00ef4225aae0fd5e215f1ec41
Reno <singularitti@outlook.com>
2022-08-17 03:49:06 -0400

    Rewrite `DirStructure` to `SubDir`, rename `iofiles` to `getfiles`

--- src/Config/io.jl
+++ src/Config/io.jl
@@ -1,9 +1,6 @@
 using Configurations: @option
 using Formatting: sprintf1
 
-export DirStructure
-export iofiles
-
 @option struct InputFile
     name::String = "%s.in"
 end
@@ -12,25 +9,15 @@ end
     name::String = "%s.out"
 end
 
-@option struct DirStructure
-    rootdir::String = pwd()
-    subdirname::String = "%s"
+@option struct SubDir
+    root::String = pwd()
+    name::String = "%s"
     input::InputFile = InputFile()
     output::OutputFile = OutputFile()
-    DirStructure(rootdir, subdirname, input, output) =
-        new(abspath(expanduser(rootdir)), subdirname, input, output)
 end
 
-function iofiles(ds::DirStructure, dirnames, filenames)
-    dirs = map(dirnames) do dirname
-        joinpath(ds.rootdir, sprintf1(ds.subdirname, dirname))
-    end
-    if length(filenames) == 1
-        filename = fill(only(filenames), length(dirnames))
-    end
-    return map(dirs, filenames) do dir, filename
-        input, output =
-            sprintf1(ds.input.name, filename), sprintf1(ds.output.name, filename)
-        joinpath(dir, input) => joinpath(dir, output)
-    end
+function getfiles(dir::SubDir, name, filename)
+    path = joinpath(dir.root, sprintf1(dir.name, name))
+    input, output = sprintf1(dir.input.name, filename), sprintf1(dir.output.name, filename)
+    return joinpath(path, input) => joinpath(path, output)
 end
