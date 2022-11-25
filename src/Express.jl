502ac6f83dacdd3039de5bf116590368ba158bfa
singularitti <singularitti@outlook.com>
2022-11-25 12:53:51 -0500

    Comment StaticElasticity/StaticElasticity.jl

--- src/Express.jl
+++ src/Express.jl
@@ -7,7 +7,7 @@ include("Config/Config.jl")
 include("actions.jl")
 # include("ConvergenceTestWorkflow/ConvergenceTestWorkflow.jl")
 include("EquationOfStateWorkflow/EquationOfStateWorkflow.jl")
-include("StaticElasticity/StaticElasticity.jl")
+# include("StaticElasticity/StaticElasticity.jl")
 # include("PhononWorkflow/PhononWorkflow.jl")
 # include("QuasiHarmonicApproxWorkflow/QuasiHarmonicApproxWorkflow.jl")
 
