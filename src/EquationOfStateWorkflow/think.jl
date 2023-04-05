845dbe42a9b1b49a42a5d41ff32b43842493d23d
singularitti <singularitti@outlook.com>
2023-04-05 02:05:10 -0400

    Update src/EquationOfStateWorkflow/think.jl

--- src/EquationOfStateWorkflow/think.jl
+++ src/EquationOfStateWorkflow/think.jl
@@ -7,14 +7,19 @@ using .Config: RuntimeConfig
 
 import ..Express: think
 
-# MakeInput
-function think(f::MakeInput{Scf}, files, template::Input, pressures, eos::PressureEquation)
+function think(
+    makeinput::MakeInput{Scf}, files, template::Input, pressures, eos::PressureEquation
+)
     return map(files, pressures) do (input, _), pressure
-        Thunk(f, input, template, pressure, eos)
+        Thunk(makeinput, input, template, pressure, eos)
     end
 end
 function think(
-    f::MakeInput{<:Optimization}, files, template::Input, pressures, eos::PressureEquation
+    makeinput::MakeInput{<:Optimization},
+    files,
+    template::Input,
+    pressures,
+    eos::PressureEquation,
 )
     return map(files, pressures) do (input, _), pressure
         Thunk(
@@ -22,7 +27,7 @@ function think(
                 eos = PressureEquation(
                     FitEquationOfState{Scf}()(last.(files), EnergyEquation(eos))
                 )
-                return f(file, template, pressure, eos)
+                return makeinput(file, template, pressure, eos)
             end,
             input,
             template,
@@ -31,52 +36,39 @@ function think(
         )
     end
 end
-function think(f::MakeInput, files, template::Input, volumes)
+function think(makeinput::MakeInput, files, template::Input, volumes)
     return map(files, volumes) do (input, _), volume
-        Thunk(f, input, template, volume)
+        Thunk(makeinput, input, template, volume)
     end
 end
-function think(f::MakeInput, config::NamedTuple)
+function think(makeinput::MakeInput, config::NamedTuple)
     if dimension(first(config.fixed)) == dimension(u"m^3")  # Volumes
-        think(f, config.files, config.template, config.fixed)
+        think(makeinput, config.files, config.template, config.fixed)
     else  # Pressures
-        return think(f, config.files, config.template, config.fixed, config.trial_eos)
+        return think(
+            makeinput, config.files, config.template, config.fixed, config.trial_eos
+        )
     end
 end
-function think(x::GetRawData{Scf}, config::NamedTuple)
+function think(save::SaveVolumeEnergy, config::NamedTuple)
     return Thunk(function ()
-        @show config.files
-        data = x(last.(config.files))
-        dict = isfile(config.save.ev) ? load(config.save.ev) : Dict()
-        dict[string(nameof(Scf))] = data
-        save(config.save.ev, dict)
-        return data
+        data = save(last.(config.files))
+        return save(config.save.ev, data)
     end)
 end
-function think(x::GetRawData{T}, config::NamedTuple) where {T<:Optimization}
-    return Thunk(function ()
-        data = x(last.(config.scffiles))
-        dict = isfile(config.save.ev) ? load(config.save.ev) : Dict()
-        dict[string(nameof(T))] = data
-        save(config.save.ev, dict)
-        return data
-    end)
-end
-# FitEos
-function think(x::FitEquationOfState, config::NamedTuple)
+function think(fit::FitEquationOfState, config::NamedTuple)
     return Thunk(function ()
         outputs = last.(config.files)
-        trial_eos = if calculation(x) isa Scf
+        trial_eos = if calculation(fit) isa Scf
             config.trial_eos
         else
-            JLD2.load(config.save.eos)[string(nameof(Scf))]
+            load(config.save.eos)
         end
-        eos = x(outputs, EnergyEquation(trial_eos))
-        SaveParameters{typeof(calculation(x))}()(config.save.eos, eos)
+        eos = fit(outputs, EnergyEquation(trial_eos))
+        SaveParameters{typeof(calculation(fit))}()(config.save.eos, eos)
         return eos
     end)
 end
-# Any Action
 function think(f::Action{T}, raw_config::RuntimeConfig) where {T}
     config = ExpandConfig{T}()(raw_config)
     return think(f, config)
