58de1c91b5a88bea408ffc049235dcee48a00472
singularitti <singularitti@outlook.com>
2023-06-12 13:58:49 -0400

    Split `build` into 2 stages in src/EquationOfStateWorkflow/Recipes.jl

--- src/EquationOfStateWorkflow/Recipes.jl
+++ src/EquationOfStateWorkflow/Recipes.jl
@@ -18,37 +18,80 @@ struct ParallelEosFittingRecipe <: Recipe
     config
 end
 
-function build(::Type{Workflow}, r::ParallelEosFittingRecipe)
-    stage₁, stage₂ = map((Scf, VariableCellOptimization)) do T
-        steps = map((
-            DownloadPotentials{T}(),
-            MakeInput{T}(),
-            RunCmd{T}(),
-            ExtractData{T}(),
-            SaveVolumeEnergy{T}(),
-            FitEquationOfState{T}(),
-            SaveParameters{T}(),
-        )) do action
-            think(action, r.config)
-        end
-        download = Job(steps[1]; name="download potentials in $T")
-        makeinputs = map(thunk -> Job(thunk; name="make input in $T"), steps[2])
-        runcmds = map(
-            thunk -> WeaklyDependentJob(thunk; name="run ab initio software in $T"),
-            steps[3],
-        )
-        extractdata = map(
-            thunk -> StronglyDependentJob(thunk; name="extract E(V) data in $T"),
-            steps[4],
-        )
-        savedata = StronglyDependentJob(steps[5]; name="save E(V) data in $T")
-        fiteos = StronglyDependentJob(steps[6]; name="fit E(V) data in $T")
-        saveparams = StronglyDependentJob(steps[7]; name="save EOS parameters in $T")
-        download .→ makeinputs .→ runcmds .→ extractdata .→ fiteos → saveparams
-        extractdata .→ savedata
-        Workflow(download)
+function stage(::Scf, r::ParallelEosFittingRecipe)
+    steps = map((
+        DownloadPotentials{Scf}(),
+        MakeInput{Scf}(),
+        RunCmd{Scf}(),
+        ExtractData{Scf}(),
+        SaveVolumeEnergy{Scf}(),
+        FitEquationOfState{Scf}(),
+        SaveParameters{Scf}(),
+    )) do action
+        think(action, r.config)
     end
-    stage₁ → stage₂
+    download = Job(steps[1]; name="download potentials")
+    makeinputs = map(thunk -> Job(thunk; name="make input in SCF"), steps[2])
+    runcmds = map(
+        thunk -> WeaklyDependentJob(thunk; name="run ab initio software in SCF"), steps[3]
+    )
+    extractdata = map(
+        thunk -> WeaklyDependentJob(thunk; name="extract E(V) data in SCF"), steps[4]
+    )
+    savedata = StronglyDependentJob(steps[5]; name="save E(V) data in SCF")
+    fiteos = StronglyDependentJob(steps[6]; name="fit E(V) data in SCF")
+    saveparams = StronglyDependentJob(steps[7]; name="save EOS parameters in SCF")
+    download .→ makeinputs .→ runcmds .→ extractdata .→ fiteos → saveparams
+    extractdata .→ savedata
+    return steps = (;
+        download=download,
+        makeinputs=makeinputs,
+        runcmds=runcmds,
+        extractdata=extractdata,
+        savedata=savedata,
+        fiteos=fiteos,
+        saveparams=saveparams,
+    )
+end
+function stage(::VariableCellOptimization, r::ParallelEosFittingRecipe)
+    steps = map((
+        MakeInput{VariableCellOptimization}(),
+        RunCmd{VariableCellOptimization}(),
+        ExtractData{VariableCellOptimization}(),
+        SaveVolumeEnergy{VariableCellOptimization}(),
+        FitEquationOfState{VariableCellOptimization}(),
+        SaveParameters{VariableCellOptimization}(),
+    )) do action
+        think(action, r.config)
+    end
+    makeinputs = map(
+        thunk -> StronglyDependentJob(thunk; name="make input in vc-relax"), steps[2]
+    )
+    runcmds = map(
+        thunk -> WeaklyDependentJob(thunk; name="run ab initio software in vc-relax"),
+        steps[3],
+    )
+    extractdata = map(
+        thunk -> WeaklyDependentJob(thunk; name="extract E(V) data in vc-relax"), steps[4]
+    )
+    savedata = StronglyDependentJob(steps[5]; name="save E(V) data in vc-relax")
+    fiteos = StronglyDependentJob(steps[6]; name="fit E(V) data in vc-relax")
+    saveparams = StronglyDependentJob(steps[7]; name="save EOS parameters in vc-relax")
+    makeinputs .→ runcmds .→ extractdata .→ fiteos → saveparams
+    extractdata .→ savedata
+    return steps = (;
+        makeinputs=makeinputs,
+        runcmds=runcmds,
+        extractdata=extractdata,
+        savedata=savedata,
+        fiteos=fiteos,
+        saveparams=saveparams,
+    )
+end
+
+function build(::Type{Workflow}, r::ParallelEosFittingRecipe)
+    stage₁, stage₂ = stage(Scf, r), stage(VariableCellOptimization, r)
+    stage₁.fiteos .→ stage₂.makeinputs
     return Workflow(first(eachjob(stage₁)))
 end
 function build(::Type{Workflow}, file)
