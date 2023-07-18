4ffb73b278180ac55966774bd97947f312453656
singularitti <singularitti@outlook.com>
2023-07-18 02:49:45 -0400

    Move src/manager.jl to ExpressBase.jl
    See https://github.com/MineralsCloud/ExpressBase.jl/commit/26893799a1b25c6607dc2b29185d5ae4dbae3456

--- src/Express.jl
+++ src/Express.jl
@@ -2,7 +2,6 @@ module Express
 
 # include("SelfConsistentField.jl")
 # include("BandStructure.jl")
-include("manager.jl")
 include("actions.jl")
 # include("ConvergenceTestWorkflow/ConvergenceTestWorkflow.jl")
 include("EquationOfStateWorkflow/EquationOfStateWorkflow.jl")
--- src/manager.jl
+++
@@ -1,9 +0,0 @@
-export distribute_procs
-
-function distribute_procs(nprocs, jobsize)
-    quotient, remainder = divrem(nprocs, jobsize)
-    if !iszero(remainder)
-        @warn "the processes are not fully balanced! Consider the number of subjobs!"
-    end
-    return quotient
-end
