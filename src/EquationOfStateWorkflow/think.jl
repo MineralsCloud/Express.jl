824c7ccbf4c4700bab432d3c68e0c275e0b7f3ae
singularitti <singularitti@outlook.com>
2023-07-20 04:46:54 -0400

    Fix `think` with `EasyConfig.Config` in src/EquationOfStateWorkflow/think.jl

--- src/EquationOfStateWorkflow/think.jl
+++ src/EquationOfStateWorkflow/think.jl
@@ -1,26 +1,36 @@
 using Thinkers: Thunk
+using EasyConfig: Config as Conf
 using Unitful: dimension, @u_str
 
 using .Config: StaticConfig, expand
 
 import ExpressBase: think
 
-think(action::CreateInput, config::NamedTuple) =
-    collect(Thunk(action, config.template) for _ in Base.OneTo(length(config.fixed)))
-think(action::WriteInput, config::NamedTuple) = think.(action, first.(config.io))
-think(action::ExtractData, config::NamedTuple) =
-    collect(Thunk(action, file) for file in last.(config.io))
-think(action::SaveData, config::NamedTuple) = Thunk(action, config.data.raw)
-think(action::SaveParameters, config::NamedTuple) = Thunk(action, config.save.parameters)
-function think(action::FitEquationOfState, config::NamedTuple)
-    trial_eos = if action.calculation isa SCF
-        config.trial_eos
+function think(action::ComputeVolume, conf::Conf)
+    trial_eos = if action.calculation isa SelfConsistentField
+        conf.trial_eos
     else
-        loadparameters(config.save.parameters)
+        loadparameters(conf.data.eos_params)
+    end
+    return collect(Thunk(action, pressure, trial_eos) for pressure in conf.at)
+end
+think(action::CreateInput, conf::Conf) =
+    collect(Thunk(action, conf.template) for _ in Base.OneTo(length(conf.fixed)))
+think(action::WriteInput, conf::Conf) =
+    collect(think(action, file) for file in first.(conf.io))
+think(action::ExtractData, conf::Conf) =
+    collect(Thunk(action, file) for file in last.(conf.io))
+think(action::SaveData, conf::Conf) = Thunk(action, conf.data.raw)
+think(action::SaveParameters, conf::Conf) = Thunk(action, conf.data.eos_params)
+function think(action::FitEquationOfState, conf::Conf)
+    trial_eos = if action.calculation isa SelfConsistentField
+        conf.trial_eos
+    else
+        loadparameters(conf.data.eos_params)
     end
     return Thunk(action(EnergyEquation(trial_eos)), Set())
 end
-function think(action::Action, config::StaticConfig)
-    config = expand(config, action.calculation)
-    return think(action, config)
+function think(action::Action{T}, config::StaticConfig) where {T}
+    config = expand(config, T())
+    return think(action, config::Conf)
 end
