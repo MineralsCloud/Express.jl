16aa517be3f5301d39667ad738a1e752763a7f21
singularitti <singularitti@outlook.com>
2023-07-19 20:49:20 -0400

    Only leave `think` for `NamedTuple`s

--- src/EquationOfStateWorkflow/think.jl
+++ src/EquationOfStateWorkflow/think.jl
@@ -8,10 +8,10 @@ import ExpressBase: think
 think(obj::CreateInput, config::NamedTuple) =
     collect(Thunk(obj, config.template) for _ in Base.OneTo(length(config.fixed)))
 think(obj::WriteInput, config::NamedTuple) = think.(obj, first.(config.io))
-think(obj::ExtractData, files::AbstractVector) = collect(Thunk(obj, file) for file in files)
-think(obj::ExtractData, config::NamedTuple) = think(obj, last.(config.io))
-think(obj::SaveData, config::NamedTuple) = Thunk(obj(config.save.raw), Set())
-think(obj::SaveParameters, config::NamedTuple) = Thunk(obj(config.save.parameters), Set())
+think(obj::ExtractData, config::NamedTuple) =
+    collect(Thunk(obj, file) for file in last.(config.io))
+think(obj::SaveData, config::NamedTuple) = Thunk(obj, config.data.raw)
+think(obj::SaveParameters, config::NamedTuple) = Thunk(obj, config.save.parameters)
 function think(obj::FitEquationOfState, config::NamedTuple)
     trial_eos =
         obj.calculation isa SCF ? config.trial_eos : loadparameters(config.save.parameters)
