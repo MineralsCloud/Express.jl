e22a56c18687b1c5f3d4c6c5f8d2ad93ac0d4dc4
singularitti <singularitti@outlook.com>
2023-07-24 23:57:48 -0400

    Remove `jobify` methods

--- src/ConvergenceTestWorkflow/actions.jl
+++ src/ConvergenceTestWorkflow/actions.jl
@@ -13,20 +13,6 @@ struct CreateInput{T} <: Action{T}
     calculation::T
 end
 
-function jobify(x::CreateInput{SCF}, cfgfile)
-    dict = load(cfgfile)
-    config = ExpandConfig{SCF}()(dict)
-    inputs = first.(config.files)
-    if config.parameters isa AbstractVector{<:Tuple}
-        return map(inputs, config.parameters) do input, (mesh, shift)
-            Job(() -> x(input, config.template, mesh, shift, "Y-m-d_H:M:S"))
-        end
-    else
-        return map(inputs, config.parameters) do input, energy
-            Job(() -> x(input, config.template, energy, "Y-m-d_H:M:S"))
-        end
-    end
-end
 
 function parseoutput end
 
@@ -34,32 +20,12 @@ struct ExtractData{T} <: Action{T}
     calculation::T
 end
 
-function jobify(x::ExtractData{T}, cfgfile) where {T}
-    dict = load(cfgfile)
-    config = ExpandConfig{T}()(dict)
-    return Job(function ()
-        data = x(last.(config.files))
-        saved = Dict("results" => (ustrip ∘ last).(data))
-        save(config.save_raw, saved)
-        return data
-    end)
 end
 
 struct TestConvergence{T} <: Action{T}
     calculation::T
 end
 (x::TestConvergence)(data) = isconvergent(data)
-
-function jobify(x::TestConvergence{T}, cfgfile) where {T}
-    dict = load(cfgfile)
-    config = ExpandConfig{T}()(dict)
-    return Job(function ()
-        data = ExtractData{T}()(last.(config.files))
-        saved = Dict("results" => (ustrip ∘ last).(data))
-        save(config.save_raw, saved)
-        return x(data)
-    end)
-end
 
 function isconvergent(a::AbstractVector)
     terms = abs.(diff(a))
