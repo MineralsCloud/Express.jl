c7767a8d3f89fc7a8048a617a63333f345cbc887
singularitti <singularitti@outlook.com>
2023-10-15 04:02:49 -0400

    Simplify code in src/PhononWorkflow/actions.jl

--- src/PhononWorkflow/actions.jl
+++ src/PhononWorkflow/actions.jl
@@ -1,123 +1,44 @@
-using AbInitioSoftwareBase: Input, writetxt
+using AbInitioSoftwareBase: Input
 using EasyJobsBase: Job
 using ExpressBase:
     VariableCellOptimization,
     LatticeDynamics,
-    SCF,
-    DFPT,
+    SelfConsistentField,
+    DensityFunctionalPerturbationTheory,
     RealSpaceForceConstants,
     PhononDispersion,
     VDOS,
-    Action
-using ExpressBase.Files: load
+    Action,
+    DownloadPotentials,
+    RunCmd,
+    WriteInput
+using ExpressBase.Files: save
 
-using ..Express: RunCmd, distribute_procs
-using .Config: ExpandConfig
-
-import ..Express: jobify
-
-struct MakeInput{T} <: Action{T} end
-function (x::MakeInput{T})(
-    file, template::Input, args...
-) where {T<:Union{SCF,LatticeDynamics}}
-    input = x(template, args...)
-    mkpath(dirname(file))  # In case its parent directory is not created
-    writetxt(file, input)
-    return input
+struct CreateInput{T} <: Action{T}
+    calculation::T
 end
 
-function jobify(x::MakeInput{DFPT}, cfgfile)
-    dict = load(cfgfile)
-    config = ExpandConfig{DFPT}()(dict)
-    return map(config.files[2], config.files[1]) do (input, _), (previnput, _)
-        Job(function ()
-            str = read(previnput, String)
-            prev = parse(inputtype(SCF), str)
-            return x(input, config.template.dfpt, prev)
-        end)
-    end
+struct ExtractData{T} <: Action{T}
+    calculation::T
 end
-function jobify(x::MakeInput{RealSpaceForceConstants}, cfgfile)
-    dict = load(cfgfile)
-    config = ExpandConfig{RealSpaceForceConstants}()(dict)
-    return map(config.files[3], config.files[2]) do (input, _), (previnput, _)
-        Job(function ()
-            str = read(previnput, String)
-            prev = parse(inputtype(DFPT), str)
-            return x(input, config.template.q2r, prev)
-        end)
-    end
+
+struct GatherData{T} <: Action{T}
+    calculation::T
 end
-function jobify(x::MakeInput{T}, cfgfile) where {T<:Union{PhononDispersion,VDOS}}
-    dict = load(cfgfile)
-    config = ExpandConfig{T}()(dict)
-    return map(
-        config.files[4], config.files[3], config.files[2]
-    ) do (input, _), (previnput, _), (pprevinput, _)
-        Job(function ()
-            str = read(previnput, String)
-            q2r = parse(inputtype(RealSpaceForceConstants), str)
-            str = read(pprevinput, String)
-            dfpt = parse(inputtype(DFPT), str)
-            return x(input, config.template.disp, q2r, dfpt)
-        end)
-    end
+(::GatherData)(iter) = collect(iter)
+
+struct SaveData{T} <: Action{T}
+    calculation::T
 end
-function jobify(x::MakeInput{SCF}, cfgfile)
-    dict = load(cfgfile)
-    config = ExpandConfig{SCF}()(dict)
-    return map(config.files[1]) do (input, _)
-        Job(
-            function ()
-                vcout = joinpath(
-                    dirname(input), string(nameof(VariableCellOptimization)) * ".out"
-                )
-                str = read(vcout, String)
-                cell = parsecell(str)
-                if any(x === nothing for x in cell)
-                    error("set cell failed!")
-                else
-                    cell
-                end
-                return x(input, config.template.scf, first(cell), last(cell))
-            end,
-        )
-    end
+function (action::SaveData)(path, raw_data)
+    raw_data = sort(collect(raw_data))  # In case the data is not sorted
+    data = Dict(
+        "volume" => (string ∘ first).(raw_data), "energy" => (string ∘ last).(raw_data)
+    )
+    return save(path, data)
 end
+(action::SaveData)(path) = Base.Fix1(action, path)
 
 function parsecell end
 
 function inputtype end
-
-function jobify(x::RunCmd{SCF}, cfgfile)
-    dict = load(cfgfile)
-    config = ExpandConfig{SCF}()(dict)
-    np = distribute_procs(config.cli.mpi.np, length(config.files[1]))
-    return map(config.files[1]) do (input, output)
-        Job(() -> x(input, output; np=np))
-    end
-end
-function jobify(x::RunCmd{DFPT}, cfgfile)
-    dict = load(cfgfile)
-    config = ExpandConfig{DFPT}()(dict)
-    np = distribute_procs(config.cli.mpi.np, length(config.files[2]))
-    return map(config.files[2]) do (input, output)
-        Job(() -> x(input, output; np=np))
-    end
-end
-function jobify(x::RunCmd{RealSpaceForceConstants}, cfgfile)
-    dict = load(cfgfile)
-    config = ExpandConfig{RealSpaceForceConstants}()(dict)
-    np = distribute_procs(config.cli.mpi.np, length(config.files[3]))
-    return map(config.files[3]) do (input, output)
-        Job(() -> x(input, output; np=np))
-    end
-end
-function jobify(x::RunCmd{T}, cfgfile) where {T<:LatticeDynamics}
-    dict = load(cfgfile)
-    config = ExpandConfig{T}()(dict)
-    np = distribute_procs(config.cli.mpi.np, length(config.files[4]))
-    return map(config.files[4]) do (input, output)
-        Job(() -> x(input, output; np=np))
-    end
-end
