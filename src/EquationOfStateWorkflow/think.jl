b4f9d7fe368fbdb1874c52ad2a1f9c3e8e4a87f3
singularitti <singularitti@outlook.com>
2023-10-15 02:39:43 -0400

    Add `think` for `GatherData`

--- src/EquationOfStateWorkflow/think.jl
+++ src/EquationOfStateWorkflow/think.jl
@@ -23,6 +23,7 @@ think(action::WriteInput, conf::Conf) =
     collect(Thunk(action(file)) for file in first.(conf.io))
 think(action::ExtractData, conf::Conf) =
     collect(Thunk(action, file) for file in last.(conf.io))
+think(action::GatherData, ::Conf) = Thunk(action)
 think(action::SaveData, conf::Conf) = Thunk(action(conf.data.raw))
 think(action::SaveParameters, conf::Conf) = Thunk(action(conf.data.eos_params))
 function think(action::FitEquationOfState, conf::Conf)
