95ab02a78f174225d03f7088e6e1da67a484d785
singularitti <singularitti@outlook.com>
2023-11-19 15:05:50 -0500

    Add module `MolecularDynamics`

--- src/Express.jl
+++ src/Express.jl
@@ -3,6 +3,7 @@ module Express
 # include("SelfConsistentField.jl")
 # include("BandStructure.jl")
 include("ConvergenceTest/ConvergenceTest.jl")
+include("MolecularDynamics/MolecularDynamics.jl")
 include("EquationOfState/EquationOfState.jl")
 # include("StaticElasticity/StaticElasticity.jl")
 include("Phonon/Phonon.jl")
