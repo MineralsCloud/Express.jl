42e49e9896fc1d52adb531c0fab3f4876cbe88b1
singularitti <singularitti@outlook.com>
2023-11-20 03:26:07 -0500

    Fix `think` for `CreateInput`

--- src/MD/think.jl
+++ src/MD/think.jl
@@ -8,8 +8,6 @@ import ExpressBase: think
 
 think(action::CreateInput, conf::Conf) =
     collect(Thunk(action(conf.template)) for _ in Base.OneTo(length(conf.at)))
-think(action::CreateInput{<:MolecularDynamics}, conf::Conf) =
-    collect(Thunk(action, template) for template in first.(conf.io))
 think(action::ExtractCell, conf::Conf) =
     collect(Thunk(action, file) for file in last.(conf.io))
 function think(action::Action{T}, config::StaticConfig) where {T}
