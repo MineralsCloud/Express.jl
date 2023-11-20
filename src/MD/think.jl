f10abd38e1c223b1daa0ddadf89c9b204cecd6f0
singularitti <singularitti@outlook.com>
2023-11-20 00:33:36 -0500

    Add `think` for `ExtractCell`

--- src/MD/think.jl
+++ src/MD/think.jl
@@ -10,10 +10,8 @@ think(action::CreateInput, conf::Conf) =
     collect(Thunk(action(conf.template)) for _ in Base.OneTo(length(conf.at)))
 think(action::CreateInput{<:MolecularDynamics}, conf::Conf) =
     collect(Thunk(action, template) for template in first.(conf.io))
-# think(action::ExtractData, conf::Conf) =
-#     collect(Thunk(action, file) for file in last.(conf.io))
-# think(action::GatherData, ::Conf) = Thunk(action)
-# think(action::SaveData, conf::Conf) = Thunk(action(conf.data.raw))
+think(action::ExtractCell, conf::Conf) =
+    collect(Thunk(action, file) for file in last.(conf.io))
 function think(action::Action{T}, config::StaticConfig) where {T}
     config = expand(config, T())
     return think(action, config::Conf)
